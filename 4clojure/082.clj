74487bf7449e0a5b1a8d19e6465e9a2c3f44bf8e
aatack <alexanderjtatack@gmail.com>
2022-12-29 17:58:42 +0000

    Fix the outstanding bug

--- 4clojure/082.clj
+++ 4clojure/082.clj
@@ -1,5 +1,3 @@
-(require '[clojure.math.combinatorics :as combo])
-
 (defn chainable-by-deletion [left right]
   (and (= (count left) (inc (count right)))
        (some (fn [index]
@@ -10,10 +8,8 @@
 (defn chainable-by-replacement [left right]
   (and (= (count left) (count right))
        (some (fn [index]
-               (chainable-by-deletion
-                left
-                (apply str (concat (take index right)
-                                   (drop (inc index) right)))))
+               (= [(take index left) (drop (inc index) left)]
+                  [(take index right) (drop (inc index) right)]))
              (range (inc (count right))))))
 
 (defn chainable [left right]
@@ -50,11 +46,6 @@
                  (permutations (apply vector words)))))
 
 (def __ has-valid-word-chain)
-
-(filter chain-chainable
-        (permutations (apply vector #{"to" "top" "stop" "tops" "toss"})))
-
-(chainable-by-replacement "stop" "tops")
 
 (= true (__ #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"}))
 (= false (__ #{"cot" "hot" "bat" "fat"}))
