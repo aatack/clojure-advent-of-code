f1a6e61a98d64f9e414d721923f9884089691263
aatack <alexanderjtatack@gmail.com>
2022-12-29 18:01:29 +0000

    Pull everything into one big function

--- 4clojure/082.clj
+++ 4clojure/082.clj
@@ -1,49 +1,48 @@
-(defn chainable-by-deletion [left right]
-  (and (= (count left) (inc (count right)))
-       (some (fn [index]
-               (= (concat (take index left) (drop (inc index) left))
-                  (apply list right)))
-             (range (inc (count right))))))
-
-(defn chainable-by-replacement [left right]
-  (and (= (count left) (count right))
-       (some (fn [index]
-               (= [(take index left) (drop (inc index) left)]
-                  [(take index right) (drop (inc index) right)]))
-             (range (inc (count right))))))
-
-(defn chainable [left right]
-  (or (chainable-by-deletion left right)
-      (chainable-by-deletion right left)
-      (chainable-by-replacement left right)))
-
-(defn pairs [sequence]
-  (map vector (butlast sequence) (rest sequence)))
-
-(defn chain-chainable [chain]
-  (every? #(chainable (first %) (second %))
-          (pairs chain)))
-
-(defn permutations [sequence]
-  (if (< (count sequence) 2)
-    [sequence]
-    ;; TODO: find a better way of excluding duplicates
-    (set (apply
-          concat
-          (map (fn [drop-index]
-                 (let [head (take drop-index sequence)
-                       tail (drop (inc drop-index) sequence)
-                       pivot (nth sequence drop-index)]
-                   (for [permutation (permutations (concat head tail))
-                         insert-index (range (inc (count sequence)))]
-                     (concat (take insert-index permutation)
-                             [pivot]
-                             (drop insert-index permutation)))))
-               (range (count sequence)))))))
-
 (defn has-valid-word-chain [words]
-  (boolean (some chain-chainable
-                 (permutations (apply vector words)))))
+  (letfn [(chainable-by-deletion [left right]
+            (and (= (count left) (inc (count right)))
+                 (some (fn [index]
+                         (= (concat (take index left) (drop (inc index) left))
+                            (apply list right)))
+                       (range (inc (count right))))))
+
+          (chainable-by-replacement [left right]
+            (and (= (count left) (count right))
+                 (some (fn [index]
+                         (= [(take index left) (drop (inc index) left)]
+                            [(take index right) (drop (inc index) right)]))
+                       (range (inc (count right))))))
+
+          (chainable [left right]
+            (or (chainable-by-deletion left right)
+                (chainable-by-deletion right left)
+                (chainable-by-replacement left right)))
+
+          (pairs [sequence]
+            (map vector (butlast sequence) (rest sequence)))
+
+          (chain-chainable [chain]
+            (every? #(chainable (first %) (second %))
+                    (pairs chain)))
+
+          (permutations [sequence]
+            (if (< (count sequence) 2)
+              [sequence]
+              ;; TODO: find a better way of excluding duplicates
+              (set (apply
+                    concat
+                    (map (fn [drop-index]
+                           (let [head (take drop-index sequence)
+                                 tail (drop (inc drop-index) sequence)
+                                 pivot (nth sequence drop-index)]
+                             (for [permutation (permutations (concat head tail))
+                                   insert-index (range (inc (count sequence)))]
+                               (concat (take insert-index permutation)
+                                       [pivot]
+                                       (drop insert-index permutation)))))
+                         (range (count sequence)))))))]
+    (boolean (some chain-chainable
+                   (permutations (apply vector words))))))
 
 (def __ has-valid-word-chain)
 
