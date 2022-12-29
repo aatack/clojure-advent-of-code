41010090e18f29d495d4c3b94fe35cb062759dfc
aatack <alexanderjtatack@gmail.com>
2022-12-29 17:57:02 +0000

    Flesh out the rest of the functions

--- 4clojure/082.clj
+++ 4clojure/082.clj
@@ -1,3 +1,5 @@
+(require '[clojure.math.combinatorics :as combo])
+
 (defn chainable-by-deletion [left right]
   (and (= (count left) (inc (count right)))
        (some (fn [index]
@@ -8,11 +10,51 @@
 (defn chainable-by-replacement [left right]
   (and (= (count left) (count right))
        (some (fn [index]
-          (chainable-by-deletion
-           left
-           (apply str (concat (take index right)
-                              (drop (inc index) right)))))
-        (range (inc (count right))))))
+               (chainable-by-deletion
+                left
+                (apply str (concat (take index right)
+                                   (drop (inc index) right)))))
+             (range (inc (count right))))))
+
+(defn chainable [left right]
+  (or (chainable-by-deletion left right)
+      (chainable-by-deletion right left)
+      (chainable-by-replacement left right)))
+
+(defn pairs [sequence]
+  (map vector (butlast sequence) (rest sequence)))
+
+(defn chain-chainable [chain]
+  (every? #(chainable (first %) (second %))
+          (pairs chain)))
+
+(defn permutations [sequence]
+  (if (< (count sequence) 2)
+    [sequence]
+    ;; TODO: find a better way of excluding duplicates
+    (set (apply
+          concat
+          (map (fn [drop-index]
+                 (let [head (take drop-index sequence)
+                       tail (drop (inc drop-index) sequence)
+                       pivot (nth sequence drop-index)]
+                   (for [permutation (permutations (concat head tail))
+                         insert-index (range (inc (count sequence)))]
+                     (concat (take insert-index permutation)
+                             [pivot]
+                             (drop insert-index permutation)))))
+               (range (count sequence)))))))
+
+(defn has-valid-word-chain [words]
+  (boolean (some chain-chainable
+                 (permutations (apply vector words)))))
+
+(def __ has-valid-word-chain)
+
+(filter chain-chainable
+        (permutations (apply vector #{"to" "top" "stop" "tops" "toss"})))
+
+(chainable-by-replacement "stop" "tops")
 
 (= true (__ #{"hat" "coat" "dog" "cat" "oat" "cot" "hot" "hog"}))
 (= false (__ #{"cot" "hot" "bat" "fat"}))
