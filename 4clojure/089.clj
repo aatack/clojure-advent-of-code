5095a02974ce028986b2510df0dba94de4767bf9
aatack <alexanderjtatack@gmail.com>
2023-01-21 15:57:37 +0000

    Begin accounting for traversing edges backwards

--- 4clojure/089.clj
+++ 4clojure/089.clj
@@ -1,14 +1,15 @@
 (defn __ [graph]
-  (letfn [(has-path [first-edge remaining-edges]
-            (if (empty? remaining-edges)
+  (letfn [(has-path [node edges backwards]
+            (if (empty? edges)
               true
-              (some (fn [second-edge]
-                      (has-path second-edge (disj remaining-edges second-edge)))
-                    (filter #(= (first %) (second first-edge))
-                            remaining-edges))))]
+              (some (fn [edge]
+                      (has-path (if backwards (first edge) (second edge))
+                                (disj edges edge)
+                                false))
+                    (filter #(= ((if backwards first second) %) node) edges))))]
 
     (let [edges (set graph)]
-      (boolean (some #(has-path % (disj edges %)) edges)))))
+      (boolean (some #(or (has-path % edges true) (has-path % edges false)) (set (flatten graph)))))))
 
 (= true (__ [[:a :b]]))
 (= false (__ [[:a :a] [:b :b]]))
@@ -19,3 +20,5 @@
              [:b :e] [:a :d] [:b :d] [:c :e]
              [:d :e] [:c :f] [:d :f]]))
 (= false (__ [[1 2] [2 3] [2 4] [2 5]]))
+
+(__  [[:a :b] [:a :c] [:c :b]])
