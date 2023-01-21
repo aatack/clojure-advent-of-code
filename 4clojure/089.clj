c3c34f2efb15a822a9079a83af2bda86e1aba83d
aatack <alexanderjtatack@gmail.com>
2023-01-21 15:45:57 +0000

    Create a simplified version

--- 4clojure/089.clj
+++ 4clojure/089.clj
@@ -1,29 +1,14 @@
 (defn __ [graph]
-  (letfn [(pairs [sequence]
-            (map vector (butlast sequence) (rest sequence)))
+  (letfn [(has-path [first-edge remaining-edges]
+            (if (empty? remaining-edges)
+              true
+              (some (fn [second-edge]
+                      (has-path second-edge (disj remaining-edges second-edge)))
+                    (filter #(= (first %) (second first-edge))
+                            remaining-edges))))]
 
-          (permutations [sequence]
-            (if (< (count sequence) 2)
-              [sequence]
-              ;; TODO: find a better way of excluding duplicates
-              (set (apply
-                    concat
-                    (map (fn [drop-index]
-                           (let [head (take drop-index sequence)
-                                 tail (drop (inc drop-index) sequence)
-                                 pivot (nth sequence drop-index)]
-                             (for [permutation (permutations (concat head tail))
-                                   insert-index (range (inc (count sequence)))]
-                               (concat (take insert-index permutation)
-                                       [pivot]
-                                       (drop insert-index permutation)))))
-                         (range (count sequence)))))))]
-
-    (boolean (some (fn [path]
-                     (every? (fn [inputs]
-                               (= (last (first inputs)) (first (last inputs))))
-                             (pairs path)))
-                   (permutations graph)))))
+    (let [edges (set graph)]
+      (boolean (some #(has-path % (disj edges %)) edges)))))
 
 (= true (__ [[:a :b]]))
 (= false (__ [[:a :a] [:b :b]]))
