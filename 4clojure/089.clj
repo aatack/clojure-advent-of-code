d7e98f6aa47707cea443dfcbfef87d9a1cdd4f06
aatack <alexanderjtatack@gmail.com>
2023-01-22 23:08:04 +0000

    Minimise the function to make it work properly

--- 4clojure/089.clj
+++ 4clojure/089.clj
@@ -1,41 +1,29 @@
 (defn __ [graph]
-  (letfn [(has-path [node edges backwards]
+  (letfn [(drop-first [value sequence]
+            (cond
+              (empty? sequence) ()
+              (= value (first sequence)) (rest sequence)
+              :else (cons (first sequence) (drop-first value (rest sequence)))))
+
+          (has-path? [node edges]
             (if (empty? edges)
               true
               (some (fn [edge]
-                      (has-path (if backwards (first edge) (second edge))
-                                (disj edges edge)
-                                false))
-                    (filter #(= ((if backwards first second) %) node) edges))))]
+                      (let [next-node (cond
+                                        (= node (first edge)) (second edge)
+                                        (= node (second edge)) (first edge)
+                                        :else nil)]
+                        (when next-node (has-path? next-node (drop-first edge edges)))))
+                    edges)))]
 
-    (let [edges (set graph)]
-      (boolean (some #(or (has-path % edges true) (has-path % edges false)) (set (flatten graph)))))))
-
-(defn has-path? [node edges]
-  (if (empty? edges)
-    true
-    (some (fn [edge]
-            (let [next-node (cond
-                              (= node (first edge)) (second edge)
-                              (= node (second edge)) (first edge)
-                              :else nil)]
-              (when next-node (has-path? next-node (disj edges edge)))))
-          edges)))
-
-
-(has-path? 3 #{[1 2]})
-
+    (boolean (some #(has-path? % graph) (flatten graph)))))
 
 (= true (__ [[:a :b]]))
 (= false (__ [[:a :a] [:b :b]]))
-(= false (__ [[:a :b] [:a :b] [:a :c] [:c :a]
+(= false (__ [[:a :b] [:b :a] [:a :c] [:c :a]
               [:a :d] [:b :d] [:c :d]]))
 (= true (__ [[1 2] [2 3] [3 4] [4 1]]))
 (= true (__ [[:a :b] [:a :c] [:c :b] [:a :e]
              [:b :e] [:a :d] [:b :d] [:c :e]
              [:d :e] [:c :f] [:d :f]]))
 (= false (__ [[1 2] [2 3] [2 4] [2 5]]))
-
-(__  [[:a :b] [:a :c] [:c :b]])
-
-;; --- Something like "some (fn [edge] (and (has node edge))) edges"
