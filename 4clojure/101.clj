21f7f9de5551ca4261eee37ab7174a32e9325514
aatack <alexanderjtatack@gmail.com>
2023-01-29 21:06:16 +0000

    Check all the test cases

--- 4clojure/101.clj
+++ 4clojure/101.clj
@@ -40,3 +40,13 @@
                       (count word)))))]
 
       (beam-search start' explore evaluate 10))))
+
+(= (__ "kitten" "sitting") 3)
+(= (__ "closure" "clojure") (__ "clojure" "closure") 1)
+(= (__ "xyx" "xyyyx") 2)
+(= (__ "" "123456") 6)
+(= (__ "Clojure" "Clojure") (__ "" "") (__ [] []) 0)
+(= (__ [1 2 3 4] [0 2 3 4 5]) 2)
+(= (__ '(:a :b :c :d) '(:a :d)) 2)
+(= (__ "ttttattttctg" "tcaaccctaccat") 10)
+(= (__ "gaattctaatctc" "caaacaaaaaattt") 9)
