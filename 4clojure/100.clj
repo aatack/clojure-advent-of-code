c3df7ae83ebaa196755a92e4bd333d7efd21f349
aatack <alexanderjtatack@gmail.com>
2023-01-29 19:35:23 +0000

    Only use numerators during division

--- 4clojure/100.clj
+++ 4clojure/100.clj
@@ -18,7 +18,11 @@
               1
               (denominator number)))]
 
-    (/ (reduce * numbers)
+    (/ (reduce * (map numerator' numbers))
        (greatest-common-denominator (map denominator' numbers)))))
 
-
+(== (__ 2 3) 6)
+(== (__ 5 3 7) 105)
+(== (__ 1/3 2/5) 2)
+(== (__ 3/4 1/6) 3/2)
+(== (__ 7 5/7 2 3/5) 210)
