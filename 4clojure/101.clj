15fb5333a0319619085a8c1a6179323290363c9a
aatack <alexanderjtatack@gmail.com>
2023-01-29 21:05:17 +0000

    Package the code properly

--- 4clojure/101.clj
+++ 4clojure/101.clj
@@ -1,40 +1,20 @@
-(defn naive-levenshtein [left right]
-  (if (= left right)
-    0
-    (letfn [(delete [index sequence]
-              (concat (take index sequence)
-                      (nthrest sequence (inc index))))
-            (insert [index character sequence]
-              (concat (take index sequence) [character] (nthrest sequence index)))
-            (modify [index character sequence]
-              (concat (take index sequence)
-                      [character]
-                      (nthrest sequence (inc index))))]
-      (let [sequences (concat (map #(delete % left) (range (count left)))
-                              (for [index (range (inc (count left)))
-                                    character (set right)]
-                                (insert index character left))
-                              (for [index (range (count left))
-                                    character (set right)]
-                                (modify index character left)))]
-        (inc (apply min (count right)
-                    (map #(naive-levenshtein % right) sequences)))))))
+(defn __ [start target]
+  (let [start' (map identity start)
+        target' (map identity target)
+        characters (set target')]
+    (letfn [(beam-search [initial-node explore evaluate space]
+              (loop [queued-nodes [initial-node]
+                     attempts 0]
+                (let [node (first queued-nodes)
+                      nodes (rest queued-nodes)]
+                  (if (= 0 (evaluate node))
+                    attempts
+                    (recur (take space (sort-by evaluate
+                                                (concat nodes
+                                                        (explore node))))
+                           (inc attempts))))))
 
-(defn beam-search [initial-node explore evaluate space]
-  (loop [queued-nodes [initial-node]
-         attempts 0]
-    (let [node (first queued-nodes)
-          nodes (rest queued-nodes)]
-      (if (= 0 (evaluate node))
-        attempts
-        (recur (take space (sort-by evaluate
-                                    (concat nodes
-                                            (explore node))))
-               (inc attempts))))))
-
-(defn levenshtein-distance [start target]
-  (let [characters (set target)]
-    (letfn [(delete [index word]
+            (delete [index word]
               (concat (take index word)
                       (nthrest word (inc index))))
             (insert [index character word]
@@ -54,11 +34,9 @@
                         (modify index character word))))
             (evaluate [word]
               (+
-               (abs (- (count word) (count target)))
+               (abs (- (count word) (count target')))
                (if (empty? word) 0
-                   (/ (reduce + (map #(if (= %1 %2) 0 1) word target))
+                   (/ (reduce + (map #(if (= %1 %2) 0 1) word target'))
                       (count word)))))]
 
-      (beam-search start explore evaluate 10))))
-
-(levenshtein-distance (map identity "gaattctaatctc") (map identity "caaacaaaaaattt"))
+      (beam-search start' explore evaluate 10))))
