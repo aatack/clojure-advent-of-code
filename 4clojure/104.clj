05c0c2718f078945bdb3efb6c9371201f082947f
aatack <alexanderjtatack@gmail.com>
2023-01-30 00:05:21 +0000

    Properly sort the possibilities

--- 4clojure/104.clj
+++ 4clojure/104.clj
@@ -1,17 +1,22 @@
 (defn __ [number]
   (let [numerals {1000 [\M]
+                  900 [\C \M]
                   500 [\D]
+                  400 [\C \D]
                   100 [\C]
+                  90 [\X \C]
                   50 [\L]
+                  40 [\X \L]
                   10 [\X]
+                  9 [\I \X]
                   5 [\V]
+                  4 [\I \V]
                   1 [\I]}]
     (loop [remaining number
            numeral []]
       (if (= remaining 0)
         (apply str numeral)
-        (let [[value sequence]
-              (first (filter (fn [[value' _]] (<= value' remaining)) numerals))]
+        (let [possibilities (filter (fn [[value' _]] (<= value' remaining)) numerals)
+              [value sequence]
+              (first (reverse (sort-by first possibilities)))]
           (recur (- remaining value) (concat numeral sequence)))))))
-
-(__ 827)
