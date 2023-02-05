753685824b31e2dc308ffc9d02b78cc7e180d7d9
aatack <alexanderjtatack@gmail.com>
2023-02-05 23:56:07 +0000

    Do not output the first element randomly

--- 4clojure/110.clj
+++ 4clojure/110.clj
@@ -1,7 +1,11 @@
-(defn __ [initial-sequence]
-  (letfn [(pronounce [sequence]
-            (->> sequence
-                 (partition-by identity)
-                 (map #(list (count %) (first %)))
-                 (apply concat)))]
-    (lazy-seq (cons initial-sequence (__ (pronounce initial-sequence))))))
+(defn __ [sequence]
+  (let [pronounced (->> sequence
+                        (partition-by identity)
+                        (map #(list (count %) (first %)))
+                        (apply concat))]
+    (lazy-seq (cons pronounced (__ pronounced)))))
+
+(= [[1 1] [2 1] [1 2 1 1]] (take 3 (__ [1])))
+(= [3 1 2 4] (first (__ [1 1 1 4 4])))
+(= [1 1 1 3 2 1 3 2 1 1] (nth (__ [1]) 6))
+(= 338 (count (nth (__ [3 2]) 15)))
