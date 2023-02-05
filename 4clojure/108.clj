160032b50e3cfbb1cdd17c12b0fdced405816e36
aatack <alexanderjtatack@gmail.com>
2023-02-05 00:03:07 +0000

    Check it passes the test cases

--- 4clojure/108.clj
+++ 4clojure/108.clj
@@ -13,3 +13,10 @@
                      tail
                      (cons head tail)))
                  (map vector heads tails))))))
+
+(= 3 (__ [3 4 5]))
+(= 4 (__ [1 2 3 4 5 6 7] [0.5 3/2 4 19]))
+(= 64 (__ (map #(* % % %) (range))
+          (filter #(zero? (bit-and % (dec %))) (range))
+          (iterate inc 20)))
+(= 7 (__ (range) (range 0 100 7/6) [2 3 5 7 11 13]))
