d40736957162f8dbe6757201ea131a6b0da51bc0
aatack <alexanderjtatack@gmail.com>
2023-02-06 23:54:23 +0000

    Find matching words only

--- 4clojure/111.clj
+++ 4clojure/111.clj
@@ -5,14 +5,20 @@
             (->> line
                  (partition-by #(= % "#"))
                  (filter #(and (> (count %) 1)
-                               (not ((set %) "#"))))))]
-    
+                               (not ((set %) "#"))))))
+
+          (word-matches-space [space]
+            (and (= (count word) (count space))
+                 (every? identity (map #(or (= %1 "_") (= %1 %2))
+                                       space
+                                       (map str word)))))]
+
     (let [rows (map #(str/split % #" ") board)
           columns (apply map vector rows)
           lines (concat rows columns)
           spaces (apply concat (map spaces-from-line lines))]
-      
-      spaces)))
+
+      (boolean (some word-matches-space spaces)))))
 
 (__ "clojure" ["_ _ _ # j o y"
                "_ _ o _ _ _ _"
