3d213560842b70f4e889ee33306221e7dc88e308
aatack <alexanderjtatack@gmail.com>
2023-02-07 00:35:15 +0000

    Verify the results

--- 4clojure/115.clj
+++ 4clojure/115.clj
@@ -12,3 +12,13 @@
         length (int (/ (count digits) 2))]
     (boolean (= (apply + (take length digits))
                 (apply + (take length (reverse digits)))))))
+
+(= true (__ 11))
+(= true (__ 121))
+(= false (__ 123))
+(= true (__ 0))
+(= false (__ 88099))
+(= true (__ 89098))
+(= true (__ 89089))
+(= (take 20 (filter __ (range)))
+   [0 1 2 3 4 5 6 7 8 9 11 22 33 44 55 66 77 88 99 101])
