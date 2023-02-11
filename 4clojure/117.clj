6981ecd0f87af56999b2f2b7c8bafb472d2b43c4
aatack <alexanderjtatack@gmail.com>
2023-02-11 03:38:51 +0000

    Put the problem into context

--- 4clojure/117.clj
+++ 4clojure/117.clj
@@ -1,21 +1,47 @@
 (defn __ [maze]
   (let [height (count maze)
-        width (count (first maze))]
+        width (count (first maze))
 
-    (letfn [(depth-first-search [initial explore accept?]
-              (loop [unexplored (list initial)
-                     explored #{}]
-                (let [current (first unexplored)]
-                  (cond
-                    (explored current) (recur (rest unexplored) explored)
-                    (accept? current) current
-                    :else (recur (concat (explore current) (rest unexplored))
-                                 (conj explored current))))))]
+        lookup (fn [x y]
+                 (if (< -1 y height)
+                   (if (< -1 x width)
+                     (nth (nth maze y) x)
+                     "#")
+                   "#"))
 
-      (depth-first-search 1 #(list (inc %)) #(= % 6)))))
+        mouse (first (for [x (range 0 width)
+                           y (range 0 height)
+                           :when (= (lookup x y) \M)]
+                       [x y]))
+
+        cheese (first (for [x (range 0 width)
+                            y (range 0 height)
+                            :when (= (lookup x y) \C)]
+                        [x y]))
+
+        depth-first-search
+        (fn [initial explore accept?]
+          (loop [unexplored (list initial)
+                 explored #{}]
+            (let [current (first unexplored)]
+              (cond
+                (explored current) (recur (rest unexplored) explored)
+                (accept? current) current
+                :else (recur (concat (explore current) (rest unexplored))
+                             (conj explored current))))))]
+
+    (boolean (depth-first-search
+              mouse
+              (fn [[x y]] (if (= (lookup x y) \#)
+                            []
+                            [[(dec x) y]
+                             [(inc x) y]
+                             [x (dec y)]
+                             [x (inc y)]]))
+              #(= % cheese)))))
 
 (__ ["#######"
-     "#     #"
+     "#    ##"
      "#  #  #"
      "#M # C#"
      "#######"])
