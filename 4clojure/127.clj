2cfd773975b5fb7f974bcdf66747de5ee68837f0
aatack <alexanderjtatack@gmail.com>
2023-02-19 19:46:19 +0000

    Move away from the search idea

--- 4clojure/127.clj
+++ 4clojure/127.clj
@@ -1,8 +1,3 @@
-;; NOTE: I realise in hindsight that it would have been much simpler to have
-;;       a look for desirable structures within the existing data structure,
-;;       rather than bother implementing all the shearing behaviour; I'm too
-;;       far down this road now, though...
-
 (defn __ [ocr-output]
   (letfn [(power [number]
             (apply * (repeat number 2)))
@@ -52,36 +47,6 @@
           (rotations [rock]
             (take 4 (iterate rotate rock)))
 
-          (shear-vertical [rock vertical]
-            (-> rock
-                (transform (fn [[x y]] (when (>= x vertical) [x y])))
-                focus))
-
-          (shear-diagonal [rock diagonal]
-            (-> rock
-                (transform (fn [[x y]] (when (>= (+ x y) diagonal) [x y])))
-                focus))
-
-          (shears [rock]
-            (apply disj (->> (for [rotation (rotations rock)]
-                               (concat (take-while #(not (empty? (% :minerals)))
-                                                   (map #(shear-vertical rotation %)
-                                                        (range)))
-                                       (take-while #(not (empty? (% :minerals)))
-                                                   (map #(shear-diagonal rotation %)
-                                                        (range)))))
-                             (apply concat)
-                             (filter #(>= (count (% :minerals)) 3))
-                             set)
-                   (rotations rock)))
-
-          (total-size [rock]
-            (let [minerals (count (rock :minerals))
-                  waste (count (rock :waste))
-                  size (+ minerals waste)
-                  purity (/ minerals size)]
-              (* -1 purity)))
-
           (vertical-isosceles-size [rock [x y]]
             (let [dimension
                   (or (last (take-while (fn [size]
@@ -109,36 +74,17 @@
                       0)]
               (* dimension dimension)))
 
-          (pure? [rock]
-            (if (empty? (rock :waste))
-              (apply max (map (fn [rotation]
-                                (vertical-isosceles-size rotation [0 0]))
-                              (rotations rock)))
-              0))
+          (isosceles-size [rock point]
+            (max (vertical-isosceles-size rock point)
+                 (diagonal-isosceles-size rock point)))]
 
-          (beam-search [initial-node explore heuristic accept? beam-width]
-            (loop [queued-nodes [initial-node]
-                   attempts 0]
-              (let [node (first queued-nodes)
-                    nodes (rest queued-nodes)]
-                (cond
-                  (nil? node) nil
-                  (>= (accept? node) 4) (accept? node)
-                  :else (recur (take beam-width
-                                     (sort-by heuristic
-                                              (concat nodes (explore node))))
-                               (inc attempts))))))]
-
-    (beam-search (rotate (rotate (parse-rock ocr-output)))
-                 shears
-                 total-size
-                 pure?
-                 1000)
-    #_(-> ocr-output
-          parse-rock
-          rotate
-          rotate
-          (diagonal-isosceles-size [2 1]))))
+    (let [maximum-size
+          (apply max
+                 (apply concat
+                        (for [rotation (rotations (parse-rock ocr-output))]
+                          (map #(isosceles-size rotation %)
+                               (rotation :minerals)))))]
+      (when (>= maximum-size 3) maximum-size))))
 
 (__ [1 3 7 15 31])
 (__ [1 2])
