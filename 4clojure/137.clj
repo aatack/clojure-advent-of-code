c6756bf016229cd2aaa2a3edb91139c26ee5fdc9
aatack <alexanderjtatack@gmail.com>
2023-02-20 00:21:18 +0000

    Switch to a simpler, recursive method

--- 4clojure/137.clj
+++ 4clojure/137.clj
@@ -1,16 +1,13 @@
 (defn __ [value base]
-  (letfn [(power [exponent]
-            (apply * (repeat exponent base)))]
-
-    (loop [remaining value
-           digit-index 0
-           digit-buffer ()]
-      (if (= 0 remaining)
-        (if (empty? digit-buffer) '(0) digit-buffer)
-        (let [difference (rem remaining (power (inc digit-index)))]
-          (recur (- remaining difference)
-                 (inc digit-index)
-                 (cons (/ difference (power digit-index)) digit-buffer)))))))
+  (let [result (if (= value 0)
+                 [0]
+                 (let [digit (rem value base)]
+                   (conj (alternative (/ (- value digit) base) base)
+                         digit)))]
+    ;; Strip the leading zero if there are other digits
+    (if (= 1 (count result))
+      result
+      (rest result))))
 
 (= [1 2 3 4 5 0 1] (__ 1234501 10))
 (= [0] (__ 0 11))
