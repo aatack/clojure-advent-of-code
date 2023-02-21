a9d3b45d886e7a65b8a3e6d7652db3051f20b2aa
aatack <alexanderjtatack@gmail.com>
2023-02-21 22:23:26 +0000

    Bring in the test cases

--- 4clojure/150.clj
+++ 4clojure/150.clj
@@ -11,7 +11,7 @@
                   \8 8
                   \9 9} (str number)))
           (digits->number [digits]
-            (->> digits (apply str) Integer/parseInt))
+            (->> digits (apply str) Long/parseLong))
 
           (increment [palindrome]
             (let [[digits carry]
@@ -42,16 +42,40 @@
                           (/ (inc (count initial-digits)) 2))
 
           initial-palindrome
-          (first (filter #(>= (palindrome->number %) initial)
-                         (iterate increment
-                                  {:digits (into [] (reverse (take initial-count
-                                                          initial-digits)))
-                                   :even initial-even?})))]
+          (first (filter
+                  #(>= (palindrome->number %) initial)
+                  (iterate increment
+                           {:digits (into []
+                                          (reverse (take initial-count
+                                                         initial-digits)))
+                            :even initial-even?})))]
 
       (map palindrome->number
-             (iterate increment initial-palindrome)))))
+           (iterate increment initial-palindrome)))))
 
 (= (take 26 (__ 0))
    [0 1 2 3 4 5 6 7 8 9
     11 22 33 44 55 66 77 88 99
     101 111 121 131 141 151 161])
+
+(= (take 16 (__ 162))
+   [171 181 191 202
+    212 222 232 242
+    252 262 272 282
+    292 303 313 323])
+
+(= (take 6 (__ 1234550000))
+   [1234554321 1234664321 1234774321
+    1234884321 1234994321 1235005321])
+
+(= (first (__ (* 111111111 111111111)))
+   (* 111111111 111111111))
+
+(= (set (take 199 (__ 0)))
+   (set (map #(first (__ %)) (range 0 10000))))
+
+(= true
+   (apply < (take 6666 (__ 9999999))))
+
+(= (nth (__ 0) 10101)
+   9102019)
