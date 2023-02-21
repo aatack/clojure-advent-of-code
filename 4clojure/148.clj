0c072bc002492da4e8c51be6abf6bb9580fc67f9
aatack <alexanderjtatack@gmail.com>
2023-02-21 01:04:16 +0000

    Define the function accurately for small `n`

--- 4clojure/148.clj
+++ 4clojure/148.clj
@@ -5,6 +5,13 @@
           full-block-size (* (+ 1 (dec a) (dec b))
                              (* a b))
           blocks (int (/ n (* a b)))]
-      
+
       (+ (* half-block-size blocks)
-         (* full-block-size (triangle (dec blocks)))))))
+         (* full-block-size (triangle (dec blocks)))
+         (apply + (filter #(or (= 0 (mod % a)) (= 0 (mod % b)))
+                          (range (* blocks a b) n)))))))
+
+(= 0 (__ 3 17 11))
+(= 23 (__ 10 3 5))
+(= 233168 (__ 1000 3 5))
+(= "2333333316666668" (str (__ 100000000 3 5)))
