9381e8eb21eb1bfb1e55a9133e9677c4ea0af021
aatack <alexanderjtatack@gmail.com>
2023-02-22 00:34:59 +0000

    Ensure all combinations are caught

--- 4clojure/195.clj
+++ 4clojure/195.clj
@@ -1,12 +1,18 @@
-(defn __ [number]
-  (if (= number 0)
-    #{""}
-    (set (apply concat (for [combination (__ (dec number))]
-                         [(str "()" combination)
-                          (str "(" combination ")")
-                          (str combination "()")])))))
+(def __
+  (memoize (fn [number]
+             (if (= number 0)
+               #{""}
+               (set
+                (for [before (range number)
+                      between (range (- number before))
+                      :let [after (- (dec number) before between)]
+                      before-combination (__ before)
+                      between-combination (__ between)
+                      after-combination (__ after)]
+                  (str before-combination "("
+                       between-combination ")"
+                       after-combination)))))))
 
-((__ 9) "(((()()()())(())))")
 (= [#{""} #{"()"} #{"()()" "(())"}] (map (fn [n] (__ n)) [0 1 2]))
 (= #{"((()))" "()()()" "()(())" "(())()" "(()())"} (__ 3))
 (= 16796 (count (__ 10)))
