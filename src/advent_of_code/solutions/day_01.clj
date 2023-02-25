50287a30f359bb195a1fff7dc30525bc67e2e5b9
aatack <alexanderjtatack@gmail.com>
2023-02-25 23:15:00 +0000

    Complete the second part of the problem

--- src/advent_of_code/solutions/day_01.clj
+++ src/advent_of_code/solutions/day_01.clj
@@ -1,14 +1,21 @@
 (ns advent-of-code.solutions.day-01 
-  (:require [advent-of-code.utils :refer [load-input]]
-            [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [split-lines]]))
 
-(defn day-01a [input]
+(defn calories [input]
   (->> input
        split-lines
        (partition-by empty?)
        (take-nth 2)
        (map #(map read-string %))
-       (map #(apply + %))
-       (apply max)))
+       (map #(apply + %))))
 
-(day-01a (load-input :day-01a))
+(defn day-01a [input]
+  (->> input calories (apply max)))
+
+(defn day-01b [input]
+  (->> input
+       calories
+       sort
+       reverse
+       (take 3)
+       (apply +)))
