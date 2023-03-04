25fdeb39306ce1615b25a2b357c9d3b370a84520
aatack <alexanderjtatack@gmail.com>
2023-03-04 23:50:20 +0000

    Write short functions for splitting rucksacks into compartments

--- src/advent_of_code/solutions/day_03.clj
+++ src/advent_of_code/solutions/day_03.clj
@@ -1,5 +1,14 @@
 (ns advent-of-code.solutions.day-03 
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [split-lines]]
+            [clojure.set :refer [intersection]]))
+
+(defn compartments [rucksack]
+  (let [compartment-size (/ (count rucksack) 2)]
+    [(take compartment-size rucksack)
+     (drop compartment-size rucksack)]))
+
+(defn shared-items [left-compartment right-comparment]
+  (intersection (vector left-compartment) (vector right-comparment)))
 
 (defn day-03a [input]
   input)
