c3df0fb6f97d6c8af1075ca3a247274ed35b2907
aatack <alexanderjtatack@gmail.com>
2023-03-05 23:54:14 +0000

    Parse the stack schematic

---
+++ src/advent_of_code/solutions/day_05.clj
@@ -0,0 +1,33 @@
+(ns advent-of-code.solutions.day-05
+  (:require [clojure.string :refer [split-lines]]))
+
+(defn transpose [sequences]
+  (apply map vector sequences))
+
+(defn split-input
+  "Splits input into stack and procedure sections."
+  [input]
+  (->> input
+       split-lines
+       (partition-by empty?)
+       (take-nth 2)))
+
+(defn parse-stack [stack]
+  (->> stack
+       butlast
+       (map rest)
+       (map #(take-nth 4 %))
+       (map #(apply str %))
+       transpose
+       (map #(drop-while (fn [character] (= character \space)) %))))
+
+(defn parse-procedure [procedure])
+
+(defn day-05a [input]
+  (let [[stack-input procedure-input] (split-input input)
+        stack (parse-stack stack-input)
+        procedure (parse-procedure procedure-input)]
+    (parse-stack stack-input)))
+
+(defn day-05b [input]
+  (->> input))
