781a2dd61ca14d83ce305780cc031dcaac2ad160
aatack <alexanderjtatack@gmail.com>
2023-03-05 23:12:37 +0000

    Define each item's priority

--- src/advent_of_code/solutions/day_03.clj
+++ src/advent_of_code/solutions/day_03.clj
@@ -1,14 +1,19 @@
 (ns advent-of-code.solutions.day-03 
-  (:require [clojure.string :refer [split-lines]]
+  (:require [clojure.string :refer [split-lines upper-case]]
             [clojure.set :refer [intersection]]))
 
 (defn compartments [rucksack]
   (let [compartment-size (/ (count rucksack) 2)]
-    [(take compartment-size rucksack)
-     (drop compartment-size rucksack)]))
+    [(set (take compartment-size rucksack))
+     (set (drop compartment-size rucksack))]))
 
-(defn shared-items [left-compartment right-comparment]
-  (intersection (vector left-compartment) (vector right-comparment)))
+(defn shared-items [[left-compartment right-comparment]]
+  (intersection left-compartment right-comparment))
+
+(defn priority [item]
+  (if (= item (first (upper-case item)))
+    (+ 27 (- (int item) (int \A)))
+    (+ 1 (- (int item) (int \a)))))
 
 (defn day-03a [input]
   input)
