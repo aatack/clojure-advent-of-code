69ba70df2f2b9cfded1a48a7df39423dffefc610
aatack <alexanderjtatack@gmail.com>
2023-03-05 23:33:42 +0000

    Parse each assignment

--- src/advent_of_code/solutions/day_04.clj
+++ src/advent_of_code/solutions/day_04.clj
@@ -1,8 +1,17 @@
 (ns advent-of-code.solutions.day-04
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [split-lines split]]))
+
+(defn parse-assignment [assignment]
+  (->> (split assignment #",")
+       (map #(split % #"-"))
+       flatten
+       (map read-string)
+       (partition 2)))
 
 (defn day-04a [input]
-  (->> input))
+  (->> input
+       split-lines
+       (map parse-assignment)))
 
 (defn day-04b [input]
   (->> input))
