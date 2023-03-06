b45d52241b0a0606d2c85bdc72f7796a9081c42b
aatack <alexanderjtatack@gmail.com>
2023-03-06 00:41:44 +0000

    Generalise finding the start of message marker

--- src/advent_of_code/solutions/day_06.clj
+++ src/advent_of_code/solutions/day_06.clj
@@ -1,13 +1,16 @@
 (ns advent-of-code.solutions.day-06)
 
-(defn day-06a [input]
-  (->> input
-       (partition 4 1)
+(defn start-of-message-marker [distinct-characters buffer]
+  (->> buffer
+       (partition distinct-characters 1)
        (map set)
-       (map #(= (count %) 4))
+       (map #(= (count %) distinct-characters))
        (take-while not)
        count
-       (+ 4)))
+       (+ distinct-characters)))
+
+(defn day-06a [input]
+  (start-of-message-marker 4 input))
 
 (defn day-06b [input]
-  (->> input))
+  (start-of-message-marker 14 input))
