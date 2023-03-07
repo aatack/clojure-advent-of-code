902f1c220f112384fb93909f90585540b06b29d0
aatack <alexanderjtatack@gmail.com>
2023-03-07 21:47:58 +0000

    Solve the problem in one dimension

--- src/advent_of_code/solutions/day_08.clj
+++ src/advent_of_code/solutions/day_08.clj
@@ -8,6 +8,24 @@
        (map #(map str %))
        (map #(map read-string %))))
 
+(defn visible-from-front 
+  "Get the coordinates of trees in a row visible from the front."
+  [trees]
+  (first (reduce (fn [[coordinates tallest] [coordinate height]]
+                   (if (= height 9)
+                     (reduced [(conj coordinates coordinate) height])
+                     (if (> height tallest)
+                       [(conj coordinates coordinate) height]
+                       [coordinates tallest])))
+                 [[] -1]
+                 (map vector (range) trees))))
+
+(defn visible-from-back
+  "Get the coordinates of trees in a row visible from the back."
+  [trees]
+  (let [length (count trees)]
+    (map #(- length % 1) (visible-from-front (reverse trees)))))
+
 (defn day-08a [input]
   (->> input))
 
