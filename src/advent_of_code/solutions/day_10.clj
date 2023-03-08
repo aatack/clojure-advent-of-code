593aefc54e2c50b0df4166476b580cc76182f198
aatack <alexanderjtatack@gmail.com>
2023-03-08 01:12:47 +0000

    Extract the relevant parts

--- src/advent_of_code/solutions/day_10.clj
+++ src/advent_of_code/solutions/day_10.clj
@@ -8,11 +8,24 @@
        (mapcat (fn [command]
                  (if (= command ["noop"])
                    [0]
-                   [(read-string (second command)) 0])))
-       (reductions +)))
+                   [0 (read-string (second command))])))
+       (reductions + 1)))
+
+(defn nths [indices sequence]
+  (map #(nth sequence %) indices))
 
 (defn day-10a [input]
-  (signal-strength input))
+  (let [indices [20 60 100 140 180 220]]
+    (->> input
+         signal-strength
+         ;; By adding a dud element to the front, we conveniently
+         ;; transition to a 1-based indexing system like the one
+         ;; used in the prompt
+         (cons 0)
+         (apply vector)
+         (nths indices)
+         (map * indices)
+         (apply +))))
 
 (defn day-10b [input]
   (->> input))
