29e603d607e471d2987d85122037c76580ceb282
aatack <alexanderjtatack@gmail.com>
2023-03-09 23:54:37 +0000

    Parse output in the original format

--- src/advent_of_code/solutions/day_10.clj
+++ src/advent_of_code/solutions/day_10.clj
@@ -13,17 +13,12 @@
        (zipmap (rest (range)))))
 
 (defn nths [indices sequence]
-  (map #(nth sequence %) indices))
+  (map #(sequence %) indices))
 
 (defn day-10a [input]
   (let [indices [20 60 100 140 180 220]]
     (->> input
          signal-strength
-         ;; By adding a dud element to the front, we conveniently
-         ;; transition to a 1-based indexing system like the one
-         ;; used in the prompt
-         (cons 0)
-         (apply vector)
          (nths indices)
          (map * indices)
          (apply +))))
