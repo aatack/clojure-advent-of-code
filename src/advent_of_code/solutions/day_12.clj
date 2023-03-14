65d9ccc6752977e845db074707347f921dde0ad0
aatack <alexanderjtatack@gmail.com>
2023-03-14 23:21:10 +0000

    Apply the search to the problem

--- src/advent_of_code/solutions/day_12.clj
+++ src/advent_of_code/solutions/day_12.clj
@@ -1,16 +1,19 @@
 (ns advent-of-code.solutions.day-12
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [split-lines]]
+            [advent-of-code.utils :refer [breadth-first-search]]))
+
+(def start-height (int \a))
+(def end-height (int \z))
+(def wall-height 200)
 
 (defn parse-height-map [input]
   (->> input
        split-lines
        (map #(map (fn [character]
                     (cond
-                      ;; Ensure we can always step down from the start
-                      (= character \S) 100
-                      ;; Ensure we can never step down to the end
-                      (= character \E) -1
-                      :else (- (int character) (int \a))))
+                      (= character \S) start-height
+                      (= character \E) end-height
+                      :else (int character)))
                   %))
        (map #(apply vector %))
        (apply vector)))
@@ -18,20 +21,31 @@
 (defn height [height-map coordinate]
   ;; Surround the "playable" area with a wall that's too high to
   ;; escape
-  (get-in height-map coordinate 200))
+  (get-in height-map coordinate wall-height))
 
-(defn starting-coordinates [height-map]
+(defn find-coordinates [height-map value]
   (first (for [row (range (count height-map))
         column (range (count (first height-map)))
         :let [coordinate [row column]]
-        :when (= (height height-map coordinate) 100)
+        :when (= (height height-map coordinate) value)
         ]
     coordinate)))
 
 (defn day-12a [input]
-  (->> input
-       parse-height-map
-       starting-coordinates))
+  (let [height-map (parse-height-map input)
+        start (find-coordinates height-map start-height)
+        end (find-coordinates height-map end-height)
+        path (breadth-first-search
+              start
+              (fn [[x y]]
+                (let [maximum-height (inc (height height-map [x y]))]
+                  (filter #(<= (height height-map %) maximum-height)
+                          [[(inc x) y]
+                           [(dec x) y]
+                           [x (inc y)]
+                           [x (dec y)]])))
+              #(= % end))]
+    (count path)))
 
 (defn day-12b [input]
   (->> input))
