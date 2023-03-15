60b7cef73438863dc15515824189243857495419
aatack <alexanderjtatack@gmail.com>
2023-03-15 01:35:29 +0000

    Extract the beacon and sensor positions

--- src/advent_of_code/solutions/day_15.clj
+++ src/advent_of_code/solutions/day_15.clj
@@ -1,8 +1,21 @@
 (ns advent-of-code.solutions.day-15
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.string :refer [split-lines replace split]]))
+
+(defn parse-sensors-and-beacons [input]
+  (->> (-> input
+           (replace #"," "")
+           (replace #":" "")
+           (replace #"x=" "")
+           (replace #"y=" "")
+           split-lines)
+       (map #(split % #" "))
+       (map #(filter integer? (map read-string %)))
+       (map (fn [[sensor-x sensor-y beacon-x beacon-y]]
+              [[sensor-x sensor-y] [beacon-x beacon-y]]))))
 
 (defn day-15a [input]
-  (->> input))
+  (->> input
+       parse-sensors-and-beacons))
 
 (defn day-15b [input]
   (->> input))
