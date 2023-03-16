65a5c13d7c78553e20d143ca4cbea1fcff47c654
aatack <alexanderjtatack@gmail.com>
2023-03-16 02:00:17 +0000

    Map each sensor to its excluded range

--- src/advent_of_code/solutions/day_15.clj
+++ src/advent_of_code/solutions/day_15.clj
@@ -1,5 +1,6 @@
 (ns advent-of-code.solutions.day-15
-  (:require [clojure.string :refer [split-lines replace split]]))
+  (:require [clojure.string :refer [split-lines replace split]]
+            [advent-of-code.utils :refer [conj-range]]))
 
 (defn parse-sensors-and-beacons [input]
   (->> (-> input
@@ -9,14 +10,19 @@
            (replace #"y=" "")
            split-lines)
        (map #(split % #" "))
-       (map #(filter integer? (map read-string %)))
-       (map (fn [[sensor-x sensor-y beacon-x beacon-y]]
-              {:sensor {:x sensor-x :y sensor-y}
-               :beacon {:x beacon-x :y beacon-y}}))))
+       (map #(filter integer? (map read-string %)))))
+
+(defn exclusion-range [[sensor-x sensor-y beacon-x beacon-y] y]
+  (let [distance (+ (abs (- sensor-x beacon-x))
+                    (abs (- sensor-y beacon-y)))
+        width (- distance (abs (- sensor-y y)))]
+    (when (>= width 0)
+      [(- sensor-x width) (+ sensor-x width)])))
 
 (defn day-15a [input]
   (->> input
-       parse-sensors-and-beacons))
+       parse-sensors-and-beacons
+       (map #(exclusion-range % 10))))
 
 (defn day-15b [input]
   (->> input))
