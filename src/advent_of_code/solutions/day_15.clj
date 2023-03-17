a55bc04d8153c2631f91ca5d60b682ca8bda2836
aatack <alexanderjtatack@gmail.com>
2023-03-17 00:34:09 +0000

    Solve the second part the slow way

--- src/advent_of_code/solutions/day_15.clj
+++ src/advent_of_code/solutions/day_15.clj
@@ -12,17 +12,23 @@
        (map #(split % #" "))
        (map #(filter integer? (map read-string %)))))
 
-(defn exclusion-range [[sensor-x sensor-y beacon-x beacon-y] y]
+(defn sensor-exclusion-range [[sensor-x sensor-y beacon-x beacon-y] y]
   (let [distance (+ (abs (- sensor-x beacon-x))
                     (abs (- sensor-y beacon-y)))
         width (- distance (abs (- sensor-y y)))]
     (when (>= width 0)
       [(- sensor-x width) (+ sensor-x width)])))
 
+(defn sensors-exclusion-range [sensors y]
+  (->> sensors
+       (map #(sensor-exclusion-range % y))
+       (filter identity)
+       (reduce conj-range [])))
+
 (defn day-15a [input]
   (->> input
        parse-sensors-and-beacons
-       (map #(exclusion-range % 2000000))
+       (map #(sensor-exclusion-range % 2000000))
        (filter identity)
        (reduce conj-range [])
        count-ranges
@@ -31,4 +37,14 @@
        dec))
 
 (defn day-15b [input]
-  (->> input))
+  (let [sensors (parse-sensors-and-beacons input)
+        ;; This is slow - and it's not elegant - but it's not *that*
+        ;; slow
+        rows (for [y (range 4000000)
+                   :let [exclusion-range
+                         (sensors-exclusion-range sensors y)]
+                   :when (> (count exclusion-range) 1)]
+               [y exclusion-range])]
+    (let [[y [[_ dec-x] [_ _]]] (first rows)
+          x (inc dec-x)]
+      (+ (* x 4000000) y))))
