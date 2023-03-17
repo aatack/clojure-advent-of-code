00ed8f0de7fabc5a0a8afa5f4ce1284b2615d427
aatack <alexanderjtatack@gmail.com>
2023-03-17 01:06:27 +0000

    Modify the state by opening a valve

--- src/advent_of_code/solutions/day_16.clj
+++ src/advent_of_code/solutions/day_16.clj
@@ -15,16 +15,16 @@
                             (replace "; tunnel leads to valve" "")
                             (split-lines))))]
     {:valve "AA"
-   :open #{}
-   :minutes 30
-   :relieved 0
-   :valves (zipmap (map first valves)
-                   (map (fn [[_ pressure tunnels]]
-                          {:pressure pressure
-                           :tunnels tunnels})
-                        valves))}))
+     :open #{}
+     :minutes 30
+     :relieved 0
+     :valves (zipmap (map first valves)
+                     (map (fn [[_ pressure tunnels]]
+                            {:pressure pressure
+                             :tunnels tunnels})
+                          valves))}))
 
-(defn relieve-state
+(defn relieve
   "Relive pressure from all open valves."
   [state]
   (update state
@@ -35,10 +35,17 @@
                             (get-in state [:valves valve :pressure]))
                           (state :open))))))
 
+(defn open
+  "Open the current valve."
+  [state]
+  (-> state
+      (update :open #(conj % (state :valve)))
+      (update :minutes dec)))
+
 (defn day-16a [input]
   (->> input
        parse-state
-       relieve-state))
+       open))
 
 (defn day-16b [input]
   (->> input))
