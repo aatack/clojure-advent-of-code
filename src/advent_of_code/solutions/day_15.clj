0108a463deb1c2c389200cc10008eefdeabc149d
aatack <alexanderjtatack@gmail.com>
2023-03-17 00:43:33 +0000

    Describe how the problem could be solved properly

--- src/advent_of_code/solutions/day_15.clj
+++ src/advent_of_code/solutions/day_15.clj
@@ -36,7 +36,27 @@
        ;; which - for whatever reason - isn't counted in the total
        dec))
 
-(defn day-15b [input]
+(defn day-15b
+  "Find the only location that the sensor can be.
+   
+   This uses a simple brute force search.  To solve it more efficiently,
+   define a data structure to represent a diamond (ie. rectangle, but
+   aligned along the diagonals rather than along the axes).  Each sensor
+   represents a hole carved out of the remaining possibilities, whose
+   shape is also a diamond.
+   
+   Start with a diamond that encompasses the whole area, and then for
+   each sensor, split the diamond into five: first along two parallel
+   edges, then along the other two.  Discard the fifth, innermost
+   diamond.  The remaining pieces can still all be expressed in terms
+   of the diamond data structure.
+   
+   As the sensors are worked through, some of the remaining pieces of
+   the possible space of beacons will be carved away to nothing, and
+   can be removed.  In principle, at the end, there will only be one
+   diamond, which will have an area of 1.  Those are the coordinates of
+   the beacon."
+  [input]
   (let [sensors (parse-sensors-and-beacons input)
         ;; This is slow - and it's not elegant - but it's not *that*
         ;; slow
---
+++ src/advent_of_code/solutions/day_16.clj
@@ -0,0 +1,8 @@
+(ns advent-of-code.solutions.day-16
+  (:require [clojure.string :refer [split-lines]]))
+
+(defn day-16a [input]
+  (->> input))
+
+(defn day-16b [input]
+  (->> input))
