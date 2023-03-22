57da55e7bd52126c7eefc5d012d5ecd803ee275e
aatack <alexanderjtatack@gmail.com>
2023-03-22 00:32:40 +0000

    Define the basic operations

--- src/advent_of_code/solutions/day_17.clj
+++ src/advent_of_code/solutions/day_17.clj
@@ -8,6 +8,20 @@
    #{[0 0] [0 1] [0 2] [0 3]}
    #{[0 0] [1 0] [0 1] [1 1]}])
 
+(defn move
+  "Move a piece by the specified step."
+  [piece [dx dy]]
+  (map (fn [[x y]] [(+ x dx) (+ y dy)]) piece))
+
+(defn collides?
+  "Determine whether a piece collides with the terrain.
+   
+   Terrain should be provided as a function, or function-like
+   value, which maps `[x y]` coordinates to `nil` if that
+   coordinate is empty, or the coordinate otherwise."
+  [piece terrain]
+  (some terrain piece))
+
 (defn day-17a [input]
   (->> input))
 
