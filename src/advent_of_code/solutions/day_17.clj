a346a596ee596dbf47696c18f998b91eb96e40c7
aatack <alexanderjtatack@gmail.com>
2023-03-22 00:36:22 +0000

    Include walls in the collision function

--- src/advent_of_code/solutions/day_17.clj
+++ src/advent_of_code/solutions/day_17.clj
@@ -1,9 +1,9 @@
 (ns advent-of-code.solutions.day-17
   (:require [clojure.string :refer [split-lines]]))
 
-(def pieces 
+(def pieces
   [#{[0 0] [1 0] [2 0] [3 0]}
-   #{[0 0] [1 0] [2 0] [1 1] [1 -1]}
+   #{[0 1] [1 1] [2 1] [1 2] [1 0]}
    #{[0 0] [1 0] [2 0] [2 1] [2 2]}
    #{[0 0] [0 1] [0 2] [0 3]}
    #{[0 0] [1 0] [0 1] [1 1]}])
@@ -20,7 +20,12 @@
    value, which maps `[x y]` coordinates to `nil` if that
    coordinate is empty, or the coordinate otherwise."
   [piece terrain]
-  (some terrain piece))
+  (letfn [(wall [[x y]]
+            (or (<= y 0)
+                (< x 0)
+                (>= x 7)))]
+    (or (some wall piece)
+        (some terrain piece))))
 
 (defn day-17a [input]
   (->> input))
