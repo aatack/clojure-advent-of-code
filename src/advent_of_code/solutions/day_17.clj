b860d3ab1fb292adc2efbe815222e348e69ecb23
aatack <alexanderjtatack@gmail.com>
2023-03-22 00:28:35 +0000

    Define each of the pieces

---
+++ src/advent_of_code/solutions/day_17.clj
@@ -0,0 +1,15 @@
+(ns advent-of-code.solutions.day-17
+  (:require [clojure.string :refer [split-lines]]))
+
+(def pieces 
+  [#{[0 0] [1 0] [2 0] [3 0]}
+   #{[0 0] [1 0] [2 0] [1 1] [1 -1]}
+   #{[0 0] [1 0] [2 0] [2 1] [2 2]}
+   #{[0 0] [0 1] [0 2] [0 3]}
+   #{[0 0] [1 0] [0 1] [1 1]}])
+
+(defn day-17a [input]
+  (->> input))
+
+(defn day-17b [input]
+  (->> input))
