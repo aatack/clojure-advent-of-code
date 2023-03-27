7e3c25df0d0e4d10154f71a4fc9b994972eb8407
aatack <alexanderjtatack@gmail.com>
2023-03-27 23:39:47 +0100

    Parse the input in a convenient format

--- src/advent_of_code/solutions/day_20.clj
+++ src/advent_of_code/solutions/day_20.clj
@@ -1,8 +1,22 @@
 (ns advent-of-code.solutions.day-20
   (:require [clojure.string :refer [split-lines]]))
 
+(defn parse-input [input]
+  (let [lines (split-lines input)
+        limit (dec (count lines))]
+    (->> lines
+        (map-indexed (fn [index value]
+                       [index {:value (read-string value)
+                               :left (if (> index 0)
+                                       (dec index)
+                                       limit)
+                               :right (if (< index limit)
+                                        (inc index)
+                                        0)}]))
+        (into {}))))
+
 (defn day-20a [input]
-  (->> input))
+  (parse-input input))
 
 (defn day-20b [input]
   (->> input))
