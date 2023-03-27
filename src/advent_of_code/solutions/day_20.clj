f842f43453931538fea09b5f2b857f07fd87c067
aatack <alexanderjtatack@gmail.com>
2023-03-27 23:52:18 +0100

    Peek from one value to another

--- src/advent_of_code/solutions/day_20.clj
+++ src/advent_of_code/solutions/day_20.clj
@@ -5,18 +5,28 @@
   (let [lines (split-lines input)
         limit (dec (count lines))]
     (->> lines
-        (map-indexed (fn [index value]
-                       [index {:value (read-string value)
-                               :left (if (> index 0)
-                                       (dec index)
-                                       limit)
-                               :right (if (< index limit)
-                                        (inc index)
-                                        0)}]))
-        (into {}))))
+         (map-indexed (fn [index value]
+                        [index {:value (read-string value)
+                                :left (if (> index 0)
+                                        (dec index)
+                                        limit)
+                                :right (if (< index limit)
+                                         (inc index)
+                                         0)}]))
+         (into {}))))
+
+(defn peek [file value distance]
+  (let [direction (if (< distance 0) :left :right)]
+    (loop [current-value value
+           remaining-distance (abs distance)]
+      (if (= remaining-distance 0)
+        current-value
+        (recur (file (current-value direction))
+               (dec remaining-distance))))))
 
 (defn day-20a [input]
-  (parse-input input))
+  (let [file (parse-input input)]
+    (peek file (file 0) -37080)))
 
 (defn day-20b [input]
   (->> input))
