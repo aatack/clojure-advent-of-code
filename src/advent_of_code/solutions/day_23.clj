f0458133d0384fba27cd5187d29a8b03cb7f3250
aatack <alexanderjtatack@gmail.com>
2023-03-31 02:55:08 +0100

    Find proposals for all elves

--- src/advent_of_code/solutions/day_23.clj
+++ src/advent_of_code/solutions/day_23.clj
@@ -12,21 +12,29 @@
 (defn parse-positions [input]
   (into #{} (for [[y row] (enumerate (split-lines input))
                   [x cell] (enumerate row)
-                  :when (= cell \.)]
+                  :when (not= cell \.)]
               [x y])))
 
 (defn diagonals [direction]
   (for [step [-1 0 1]]
     (apply vector (map #(if (= % 0) step %) direction))))
 
-(defn proposal [state position direction-order]
-  (first (for [direction (take 4 direction-order)
+(defn proposal [state position priority]
+  (first (for [direction (take 4 priority)
                :when (every? #(not (state (add position %))) (diagonals direction))]
            (add position direction))))
 
+(defn proposals [state priority]
+  (reduce (fn [results position]
+            (let [position-proposal (proposal state position priority)]
+              (if position-proposal
+                (update results position-proposal #(conj (or % ()) position))
+                results)))
+          {}
+          state))
+
 (defn day-23a [input]
-  (->> input
-       parse-positions))
+  (proposals (parse-positions input) (directions)))
 
 (defn day-23b [input]
   (->> input))
