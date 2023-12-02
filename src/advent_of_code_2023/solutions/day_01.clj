3423aa2c5a694ec158688fcb0ddce3e19f8579a1
aatack <alexanderjtatack@gmail.com>
2023-12-02 13:44:27 +0000

    Define a set of lookups

--- src/advent_of_code_2023/solutions/day_01.clj
+++ src/advent_of_code_2023/solutions/day_01.clj
@@ -4,7 +4,7 @@
 
 (defn calibration-value [characters]
   (let [numbers (->> characters
-                      (filter #(re-matches #"[0-9]" (str %))))]
+                     (filter #(re-matches #"[0-9]" (str %))))]
     (read-string (str (first numbers) (last numbers)))))
 
 (defn day-01a [input]
@@ -12,6 +12,30 @@
        split-lines
        (map calibration-value)
        (apply +)))
+
+(def lookups {"0" 0
+              "1" 1
+              "2" 2
+              "3" 3
+              "4" 4
+              "5" 5
+              "6" 6
+              "7" 7
+              "8" 8
+              "9" 9
+              "zero" 0
+              "one" 1
+              "two" 2
+              "three" 3
+              "four" 4
+              "five" 5
+              "six" 6
+              "seven" 7
+              "eight" 8
+              "nine" 9})
+
+(defn spelled-calibration-value [characters]
+  ,,,)
 
 (defn day-01b [input]
   (->> input))
