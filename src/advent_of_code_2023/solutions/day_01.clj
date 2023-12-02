36e74e9c5417146e33f8b80170d6fc090aabc9c9
aatack <alexanderjtatack@gmail.com>
2023-12-02 13:40:33 +0000

    Complete part one

--- src/advent_of_code_2023/solutions/day_01.clj
+++ src/advent_of_code_2023/solutions/day_01.clj
@@ -2,8 +2,16 @@
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
   (:require [clojure.string :refer [split-lines]]))
 
+(defn calibration-value [characters]
+  (let [numbers (->> characters
+                      (filter #(re-matches #"[0-9]" (str %))))]
+    (read-string (str (first numbers) (last numbers)))))
+
 (defn day-01a [input]
-  (->> input))
+  (->> input
+       split-lines
+       (map calibration-value)
+       (apply +)))
 
 (defn day-01b [input]
   (->> input))
