638cb317f1861fc7ffe0db7faf135da33e084374
aatack <alexanderjtatack@gmail.com>
2023-12-02 13:33:44 +0000

    Load the correct file

---
+++ src/advent_of_code_2023/solutions/day_01.clj
@@ -0,0 +1,9 @@
+(ns advent-of-code-2023.solutions.day-01
+  #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
+  (:require [clojure.string :refer [split-lines]]))
+
+(defn day-01a [input]
+  (->> input))
+
+(defn day-01b [input]
+  (->> input))
