d54b2547e2d389b61ee543fc7f31b3d1e210caab
aatack <alexanderjtatack@gmail.com>
2023-12-03 23:35:02 +0000

    Define a common function for parsing grids

---
+++ src/advent_of_code_2023/parsing.clj
@@ -0,0 +1,14 @@
+(ns advent-of-code-2023.parsing
+  (:require [advent-of-code-2022.utils :refer [enumerate]]
+            [clojure.string :refer [split-lines]]))
+
+(defn parse-grid
+  "Parse a grid, where line breaks in the string represent new rows.
+   
+   Returns a mapping from coordinates to characters, where the x-coordinate is the
+   column and the y-coordinate is the row."
+  [characters]
+  (into {}
+        (for [[row line] (enumerate (split-lines characters))
+              [column character] (enumerate line)]
+          [[column row] character])))
