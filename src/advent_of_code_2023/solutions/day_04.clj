84dcb704f58e5652482eba0cd02a17d52c86b84d
aatack <alexanderjtatack@gmail.com>
2023-12-04 20:44:23 +0000

    Set up day four

---
+++ src/advent_of_code_2023/solutions/day_04.clj
@@ -0,0 +1,9 @@
+(ns advent-of-code-2023.solutions.day-04
+  #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
+  (:require [clojure.string :refer [split-lines]]))
+
+(defn day-04a [input]
+  (->> input))
+
+(defn day-04b [input]
+  (->> input))
