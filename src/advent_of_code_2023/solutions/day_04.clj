f15f575f76f5a1d02ffd65350a64c3e7090b0282
aatack <alexanderjtatack@gmail.com>
2023-12-04 23:29:06 +0000

    Parse the input into a convenient format

--- src/advent_of_code_2023/solutions/day_04.clj
+++ src/advent_of_code_2023/solutions/day_04.clj
@@ -1,9 +1,19 @@
 (ns advent-of-code-2023.solutions.day-04
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [advent-of-code-2023.utils :refer [split-string]]
+            [clojure.string :refer [split-lines]]))
+
+(defn parse-card [card]
+  (let [[id numbers] (split-string ":" card)
+        [winning drawn] (split-string "\\|" numbers)]
+    {:id (->> id (split-string " ") second read-string)
+     :drawn (->> drawn (split-string " ") (map read-string) (into #{}))
+     :winning (->> winning (split-string " ") (map read-string) (into #{}))}))
 
 (defn day-04a [input]
-  (->> input))
+  (->> input
+       split-lines
+       (map parse-card)))
 
 (defn day-04b [input]
   (->> input))
--- src/advent_of_code_2023/utils.clj
+++ src/advent_of_code_2023/utils.clj
@@ -1,7 +1,10 @@
 (ns advent-of-code-2023.utils
-  (:require [clojure.string :as string]))
+  (:require [clojure.string :as string :refer [split]]))
 
 (defn load-input [problem]
   (slurp (str "src/advent_of_code_2023/inputs/"
               (string/replace (name problem) "-" "_")
               ".txt")))
+
+(defn split-string [pattern string]
+  (filter not-empty (split string (re-pattern pattern))))
