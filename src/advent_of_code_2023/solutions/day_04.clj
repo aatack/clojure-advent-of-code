e44f03230578bfd1caf5ef2e9e84edc0367b667c
aatack <alexanderjtatack@gmail.com>
2023-12-04 23:32:54 +0000

    Determine each card's score

--- src/advent_of_code_2023/solutions/day_04.clj
+++ src/advent_of_code_2023/solutions/day_04.clj
@@ -1,6 +1,8 @@
 (ns advent-of-code-2023.solutions.day-04
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [advent-of-code-2023.utils :refer [split-string]]
+  (:require [advent-of-code-2022.utils :refer [power]]
+            [advent-of-code-2023.utils :refer [split-string]]
+            [clojure.set :refer [intersection]]
             [clojure.string :refer [split-lines]]))
 
 (defn parse-card [card]
@@ -9,6 +11,12 @@
     {:id (->> id (split-string " ") second read-string)
      :drawn (->> drawn (split-string " ") (map read-string) (into #{}))
      :winning (->> winning (split-string " ") (map read-string) (into #{}))}))
+
+(defn points [card]
+  (let [winning (intersection (:winning card) (:drawn card))]
+    (if (seq winning)
+      (->> winning count dec (power 2))
+      0)))
 
 (defn day-04a [input]
   (->> input
