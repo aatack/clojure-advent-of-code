297b6b6cc790a4adfc3be1c184821ef6c343d2a3
aatack <alexanderjtatack@gmail.com>
2023-12-06 00:17:14 +0000

    Parse the input into a convenient structure

--- src/advent_of_code_2023/solutions/day_05.clj
+++ src/advent_of_code_2023/solutions/day_05.clj
@@ -1,9 +1,28 @@
 (ns advent-of-code-2023.solutions.day-05
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [advent-of-code-2023.parsing :refer [parse-chunks]]
+            [advent-of-code-2023.utils :refer [split-string]]
+            [clojure.string :refer [split-lines]]))
+
+(defn parse-mapping [lines]
+  (->> lines rest (map #(str "[" % "]")) (map read-string)
+       (map #(zipmap [:source :destination :length] %))))
+
+(defn parse-input [input]
+  (let [chunks (parse-chunks input)]
+    {:seeds (->> chunks
+                 first
+                 first
+                 (split-string "\\:")
+                 second
+                 (split-string " ")
+                 (map read-string)
+                 )
+     :mappings (->> chunks rest (map parse-mapping))}))
 
 (defn day-05a [input]
-  (->> input))
+  (->> input
+       parse-input))
 
 (defn day-05b [input]
   (->> input))
