ab144b41e82a906b0a6512423ff46043d9985668
aatack <alexanderjtatack@gmail.com>
2023-12-07 00:42:05 +0000

    Start on day 6

---
+++ src/advent_of_code_2023/solutions/day_06.clj
@@ -0,0 +1,9 @@
+(ns advent-of-code-2023.solutions.day-06
+  #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
+  (:require [clojure.string :refer [split-lines]]))
+
+(defn day-06a [input]
+  (->> input))
+
+(defn day-06b [input]
+  (->> input))
