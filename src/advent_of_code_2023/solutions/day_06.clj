1e7198c796a2d7cc5d40eda6dcc2243570e2e692
aatack <alexanderjtatack@gmail.com>
2023-12-07 01:02:25 +0000

    Solve the second part

--- src/advent_of_code_2023/solutions/day_06.clj
+++ src/advent_of_code_2023/solutions/day_06.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2023.solutions.day-06
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [clojure.math :refer [sqrt]]
+            [clojure.string :refer [split-lines]]))
 
 (defn distance [charge-time race-time]
   (* (- race-time charge-time) charge-time))
@@ -17,11 +18,32 @@
 
 (* (count (winning-charge-times 7 9))
    (count (winning-charge-times 15 40))
-   (count (winning-charge-times 30 200))
-   )
+   (count (winning-charge-times 30 200)))
 
 (defn day-06a [input]
   (->> input))
+
+(defn required-charge-time
+  "Find the charge time required to win the race.
+   
+   The winning distance `d`, charge time `c`, and race time `r` must satisfy the
+   following inequality:
+   
+     d > (r - c)c ==> c^2 - rc + d > 0
+   
+   Hence we can solve for `c` to find the two roots, which are the charge times between
+   which the race will be won."
+  [race-time race-distance]
+  (let [a 1
+        b (* -1 race-time)
+        c race-distance
+
+        denominator (* 2 a)
+        location (/ (* -1 b) denominator)
+        scale (/ (sqrt (- (* b b) (* 4 a c))) denominator)]
+    (int (* 2 scale))))
+
+(required-charge-time  48989083 390110311121360)
 
 (defn day-06b [input]
   (->> input))
