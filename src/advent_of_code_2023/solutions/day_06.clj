8f98fe933208626a826c624367edbf06722feae5
aatack <alexanderjtatack@gmail.com>
2023-12-07 00:50:33 +0000

    Solve part 1

--- src/advent_of_code_2023/solutions/day_06.clj
+++ src/advent_of_code_2023/solutions/day_06.clj
@@ -2,6 +2,24 @@
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
   (:require [clojure.string :refer [split-lines]]))
 
+(defn distance [charge-time race-time]
+  (* (- race-time charge-time) charge-time))
+
+(defn winning-charge-times [race-time race-distance]
+  (->> (range (inc race-time))
+       (map #(distance % race-time))
+       (filter #(> % race-distance))))
+
+(* (count (winning-charge-times 48 390))
+   (count (winning-charge-times 98 1103))
+   (count (winning-charge-times 90 1112))
+   (count (winning-charge-times 83 1360)))
+
+(* (count (winning-charge-times 7 9))
+   (count (winning-charge-times 15 40))
+   (count (winning-charge-times 30 200))
+   )
+
 (defn day-06a [input]
   (->> input))
 
