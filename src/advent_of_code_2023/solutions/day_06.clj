fdfd3bf07bdaca6667d3b4f6faa487dc05fa529b
aatack <alexanderjtatack@gmail.com>
2023-12-07 01:07:17 +0000

    Tidy up the first part

--- src/advent_of_code_2023/solutions/day_06.clj
+++ src/advent_of_code_2023/solutions/day_06.clj
@@ -1,7 +1,13 @@
 (ns advent-of-code-2023.solutions.day-06
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [clojure.math :refer [sqrt]]
+  (:require [advent-of-code-2023.utils :refer [split-string]]
+            [clojure.math :refer [sqrt]]
             [clojure.string :refer [split-lines]]))
+
+(defn parse-races [input]
+  (let [[times distances] (split-lines input)]
+    (zipmap (->> times (split-string " ") rest (map read-string))
+            (->> distances (split-string " ") rest (map read-string)))))
 
 (defn distance [charge-time race-time]
   (* (- race-time charge-time) charge-time))
@@ -11,17 +17,12 @@
        (map #(distance % race-time))
        (filter #(> % race-distance))))
 
-(* (count (winning-charge-times 48 390))
-   (count (winning-charge-times 98 1103))
-   (count (winning-charge-times 90 1112))
-   (count (winning-charge-times 83 1360)))
-
-(* (count (winning-charge-times 7 9))
-   (count (winning-charge-times 15 40))
-   (count (winning-charge-times 30 200)))
-
 (defn day-06a [input]
-  (->> input))
+  (->> input
+       parse-races
+       (map #(apply winning-charge-times %))
+       (map count)
+       (apply *)))
 
 (defn required-charge-time
   "Find the charge time required to win the race.
