0923a3ab0eb3817c24b874e8a17c60d1a5c2bad8
aatack <alexanderjtatack@gmail.com>
2023-12-11 21:39:15 +0000

    Set up the next day

---
+++ src/advent_of_code_2023/solutions/day_10.clj
@@ -0,0 +1,9 @@
+(ns advent-of-code-2023.solutions.day-10
+  #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
+  (:require [clojure.string :refer [split-lines]]))
+
+(defn day-10a [input]
+  (->> input))
+
+(defn day-10b [input]
+  (->> input))
