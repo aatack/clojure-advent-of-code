b650556a20bca9c23b01e427f4620cf56836f997
aatack <alexanderjtatack@gmail.com>
2023-12-11 21:53:39 +0000

    Figure out the connecting coordinates for the standard pieces

--- src/advent_of_code_2023/solutions/day_10.clj
+++ src/advent_of_code_2023/solutions/day_10.clj
@@ -16,6 +16,16 @@
        first
        key))
 
+(defn connecting-coordinates [[[x y] {:keys [pipe]}]]
+  (case pipe
+    \| [[x (dec y)] [x (inc y)]]
+    \- [[(dec x) y] [(inc x) y]]
+    \L [[x (dec y)] [(inc x) y]]
+    \J [[(dec x) y] [x (dec y)]]
+    \7 [[(dec x) y] [x (inc y)]]
+    \F [[(inc x) y] [x (inc y)]]
+    \S []))
+
 (defn day-10a [input]
   (->> input
        parse-pipes
