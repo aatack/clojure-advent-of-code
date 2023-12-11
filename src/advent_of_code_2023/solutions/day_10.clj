22a0da9f022a603afdd4a598f20dba5840272361
aatack <alexanderjtatack@gmail.com>
2023-12-11 21:49:47 +0000

    Find the coordinates of the starting point

--- src/advent_of_code_2023/solutions/day_10.clj
+++ src/advent_of_code_2023/solutions/day_10.clj
@@ -3,16 +3,23 @@
   (:require [advent-of-code-2023.parsing :refer [parse-grid]]
             [clojure.string :refer [split-lines]]))
 
-(defn parse-nodes [input]
+(defn parse-pipes [input]
   (->> input
        parse-grid
        (filter (fn [[_ pipe]] (not= pipe \.)))
        (map (fn [[coordinate pipe]] [coordinate {:pipe pipe}]))
        (into {})))
 
+(defn starting-coordinates [pipes]
+  (->> pipes
+       (filter #(= (-> % val :pipe) \S))
+       first
+       key))
+
 (defn day-10a [input]
   (->> input
-       parse-nodes))
+       parse-pipes
+       starting-coordinates))
 
 (defn day-10b [input]
   (->> input))
