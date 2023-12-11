d80b030df761a3d6e9e993197b75644f9a53ae5f
aatack <alexanderjtatack@gmail.com>
2023-12-11 00:20:43 +0000

    Parse each hand

--- src/advent_of_code_2023/solutions/day_07.clj
+++ src/advent_of_code_2023/solutions/day_07.clj
@@ -1,9 +1,17 @@
 (ns advent-of-code-2023.solutions.day-07
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [advent-of-code-2023.utils :refer [split-string]]
+            [clojure.string :refer [split-lines]]))
+
+(defn parse-hands [input]
+  (->> input
+       split-lines
+       (map #(split-string " " %))
+       (map (fn [[hand bid]] {:hand (map identity hand) :big (read-string bid)}))))
 
 (defn day-07a [input]
-  (->> input))
+  (->> input
+       parse-hands))
 
 (defn day-07b [input]
   (->> input))
