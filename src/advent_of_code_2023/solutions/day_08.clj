8eef8374d1c7d3218a144a116e63dface21523c6
aatack <alexanderjtatack@gmail.com>
2023-12-11 00:59:06 +0000

    Parse the input

--- src/advent_of_code_2023/solutions/day_08.clj
+++ src/advent_of_code_2023/solutions/day_08.clj
@@ -2,8 +2,23 @@
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
   (:require [clojure.string :refer [split-lines]]))
 
+(defn parse-instructions [characters]
+  (->> characters
+       (map {\L :left \R :right})
+       cycle))
+
+(defn parse-node [characters]
+  [(subs characters 0 3) {:left (subs characters 7 10)
+                          :right (subs characters 12 15)}])
+
+(defn parse-input [input]
+  (let [lines (split-lines input)]
+    {:instructions (parse-instructions (first lines))
+     :nodes (into {} (map parse-node (drop 2 lines)))}))
+
 (defn day-08a [input]
-  (->> input))
+  (->> input
+       parse-input))
 
 (defn day-08b [input]
   (->> input))
