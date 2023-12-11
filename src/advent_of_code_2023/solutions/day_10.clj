fa163911aa4626b65bc4e90f19c530c78ea1c29b
aatack <alexanderjtatack@gmail.com>
2023-12-11 21:47:02 +0000

    Parse the input

--- src/advent_of_code_2023/solutions/day_10.clj
+++ src/advent_of_code_2023/solutions/day_10.clj
@@ -1,9 +1,18 @@
 (ns advent-of-code-2023.solutions.day-10
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [advent-of-code-2023.parsing :refer [parse-grid]]
+            [clojure.string :refer [split-lines]]))
+
+(defn parse-nodes [input]
+  (->> input
+       parse-grid
+       (filter (fn [[_ pipe]] (not= pipe \.)))
+       (map (fn [[coordinate pipe]] [coordinate {:pipe pipe}]))
+       (into {})))
 
 (defn day-10a [input]
-  (->> input))
+  (->> input
+       parse-nodes))
 
 (defn day-10b [input]
   (->> input))
