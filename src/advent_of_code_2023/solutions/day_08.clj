b4440be2d06a15fa5b2c826aa0370c1aa1c0a814
aatack <alexanderjtatack@gmail.com>
2023-12-11 01:49:46 +0000

    Determine the cycle length of each starting value

--- src/advent_of_code_2023/solutions/day_08.clj
+++ src/advent_of_code_2023/solutions/day_08.clj
@@ -1,11 +1,11 @@
 (ns advent-of-code-2023.solutions.day-08
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [advent-of-code-2022.utils :refer [enumerate]]
+            [clojure.string :refer [split-lines]]))
 
 (defn parse-instructions [characters]
   (->> characters
-       (map {\L :left \R :right})
-       cycle))
+       (map {\L :left \R :right})))
 
 (defn parse-node [characters]
   [(subs characters 0 3) {:left (subs characters 7 10)
@@ -26,7 +26,7 @@
 
 (defn day-08a [input]
   (let [{:keys [instructions nodes]} (parse-input input)]
-    (steps-required "AAA" nodes instructions 0)))
+    (steps-required "AAA" nodes (cycle instructions) 0)))
 
 (defn starting-node? [node]
   (= (last node) \A))
@@ -42,6 +42,26 @@
            (rest instructions)
            (inc steps))))
 
+(defn cycle-length
+  "Determine the cycle length of a specific starting node.
+   
+   For some reason it looks like the nodes always have a constant cycle length, with no
+   offset from the zeroth step."
+  ([node mapping instructions]
+   (cycle-length node mapping instructions 0 {}))
+  ([node mapping instructions step visits]
+   (let [identifier [node (first instructions)]]
+     (if (ending-node? node)
+       step
+       (recur (get-in mapping [node (second (first instructions))])
+              mapping
+              (rest instructions)
+              (inc step)
+              (assoc visits identifier step))))))
+
 (defn day-08b [input]
-  (let [{:keys [instructions nodes]} (parse-input input)]
-    (ghost-steps-required (filter starting-node? (keys nodes)) nodes instructions 0)))
+  (let [{:keys [instructions nodes]} (parse-input input)
+        starting-nodes (filter starting-node? (keys nodes))]
+    (->> starting-nodes
+         (map #(cycle-length % nodes (cycle (enumerate instructions))))
+         (apply *'))))
