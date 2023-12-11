85ef31901b850e017a2ee63bd92c21bc7006ff00
aatack <alexanderjtatack@gmail.com>
2023-12-11 21:35:04 +0000

    Work out the next number

--- src/advent_of_code_2023/solutions/day_09.clj
+++ src/advent_of_code_2023/solutions/day_09.clj
@@ -1,9 +1,21 @@
 (ns advent-of-code-2023.solutions.day-09
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [advent-of-code-2023.parsing :refer [parse-numbers]]
+            [clojure.string :refer [split-lines]]))
+
+(defn next-number [numbers]
+  (if (= #{0} (set numbers))
+    0
+    (+ (last numbers)
+       (next-number (->> numbers
+                         (partition 2 1)
+                         (map #(apply - (reverse %))))))))
 
 (defn day-09a [input]
-  (->> input))
+  (->> input
+       split-lines
+       (map parse-numbers)
+       (map next-number)))
 
 (defn day-09b [input]
   (->> input))
