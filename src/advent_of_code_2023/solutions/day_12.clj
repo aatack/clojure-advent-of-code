9194833628b7c8314af306c80fd0b1937210086f
aatack <alexanderjtatack@gmail.com>
2023-12-12 23:56:00 +0000

    Fix some remaining edge cases

--- src/advent_of_code_2023/solutions/day_12.clj
+++ src/advent_of_code_2023/solutions/day_12.clj
@@ -5,7 +5,7 @@
 
 (defn parse-record [record]
   (let [[history blocks] (split-string " " record)]
-    {:history (map {\. :operational \# :broken \? :unknown} history)
+    {:history (map {\. :operational \# :damaged \? :unknown} history)
      :blocks (read-string (str "[" blocks "]"))}))
 
 (defn parse-records [input]
@@ -16,33 +16,33 @@
 (defn possible-combinations [history block blocks]
   (cond
     (empty? history)
-    (if (and (empty? blocks) (or (nil? block) (= block :broken))) 1 0)
+    (if (and (empty? blocks) (or (nil? block) (= block :operational))) 1 0)
 
-    ;; (and (empty? blocks) (nil? block))
-    ;; 9999
+    (and (empty? blocks) (nil? block))
+    (if (every? #{:operational :unknown} history) 1 0)
+
+    (= (first history) :damaged)
+    (case block
+      nil (recur history (first blocks) (rest blocks))
+      :operational 0
+      1 (recur (rest history) :operational blocks)
+      (recur (rest history) (dec block) blocks))
 
     (= (first history) :operational)
     (case block
-      nil (recur history (first blocks) (rest blocks))
-      :broken 0
-      1 (recur (rest history) :broken blocks)
-      (recur (rest history) (dec block) blocks))
-
-    (= (first history) :broken)
-    (case block
-      :broken (recur (rest history) nil blocks)
+      :operational (recur (rest history) nil blocks)
       nil (recur (rest history) nil blocks)
       0)
 
     :else
-    (+ (possible-combinations (cons :operational (rest history)) block blocks)
-       (possible-combinations (cons :broken (rest history)) block blocks))))
+    (+ (possible-combinations (cons :damaged (rest history)) block blocks)
+       (possible-combinations (cons :operational (rest history)) block blocks))))
 
 (defn day-12a [input]
   (->> input
        parse-records
-       (take 1)
-       #_(map #(possible-combinations (:history %) nil (:blocks %)))))
+       (take 6)
+       (map #(possible-combinations (:history %) nil (:blocks %)))))
 
 (defn day-12b [input]
   (->> input))
