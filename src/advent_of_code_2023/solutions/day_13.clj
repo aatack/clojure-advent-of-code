0c6d7ab7e6a810d15118e004262759b5629d56ba
aatack <alexanderjtatack@gmail.com>
2023-12-14 23:59:08 +0000

    Use the more correct code for the first part too

--- src/advent_of_code_2023/solutions/day_13.clj
+++ src/advent_of_code_2023/solutions/day_13.clj
@@ -4,37 +4,6 @@
             [advent-of-code-2023.utils :refer [transpose-lines]]
             [clojure.string :refer [split-lines]]))
 
-(defn reflection-row [lines]
-  (loop [above [(first lines)]
-         below (rest lines)]
-    (cond
-      (empty? below) nil
-      (every? identity (map = above below)) (count above)
-      :else (recur (cons (first below) above) (rest below)))))
-
-(defn reflection-score [lines]
-  (let [row (reflection-row lines)]
-    (if row
-      (* 100 row)
-      (let [column (reflection-row (transpose-lines lines))]
-        (or column 0)))))
-
-(defn day-13a [input]
-  (->> input
-       parse-chunks
-       (map reflection-score)
-       (apply +)))
-
-(defn update-string [string index function]
-  (apply str (update (into [] (map identity string)) index function)))
-
-(defn changes [lines]
-  (for [row (range (count lines))
-        column (range (count (first lines)))]
-    (update lines
-            row
-            #(update-string % column {\# \., \. \#}))))
-
 (defn reflection-rows [lines]
   (for [rows (range 1 (count lines))
         :when (every? identity (map = (reverse (take rows lines)) (drop rows lines)))]
@@ -47,6 +16,27 @@
   (concat (map (fn [n] {:row n}) (reflection-rows lines))
           (map (fn [n] {:column n}) (reflection-columns lines))))
 
+(defn score [{:keys [row column]}]
+  (+ (* 100 (or row 0)) (or column 0)))
+
+(defn day-13a [input]
+  (->> input
+       parse-chunks
+       (map reflections)
+       (map first)
+       (map score)
+       (apply +)))
+
+(defn update-string [string index function]
+  (apply str (update (into [] (map identity string)) index function)))
+
+(defn changes [lines]
+  (for [row (range (count lines))
+        column (range (count (first lines)))]
+    (update lines
+            row
+            #(update-string % column {\# \., \. \#}))))
+
 (defn changed-reflection [lines]
   (let [original (into #{} (reflections lines))]
     (let [changed-reflections (->> lines
@@ -57,9 +47,6 @@
                                    (remove original))]
       (assert (= (count changed-reflections) 1))
       (first changed-reflections))))
-
-(defn score [{:keys [row column]}]
-  (+ (* 100 (or row 0)) (or column 0)))
 
 (defn day-13b [input]
   (->> input
