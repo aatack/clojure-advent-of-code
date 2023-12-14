5f98c170d44960fb37bf7aedf4b64033d16d82b9
aatack <alexanderjtatack@gmail.com>
2023-12-14 23:11:54 +0000

    Solve the first part

--- src/advent_of_code_2023/solutions/day_13.clj
+++ src/advent_of_code_2023/solutions/day_13.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2023.solutions.day-13
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
   (:require [advent-of-code-2023.parsing :refer [parse-chunks]]
+            [advent-of-code-2023.utils :refer [transpose-lines]]
             [clojure.string :refer [split-lines]]))
 
 (defn reflection-row [lines]
@@ -8,15 +9,21 @@
          below (rest lines)]
     (cond 
       (empty? below) nil
-      (every? identity (map = above below)) [(count above) above below]
+      (every? identity (map = above below)) (count above)
       :else (recur (cons (first below) above) (rest below)))))
+
+(defn reflection-score [lines]
+  (let [row (reflection-row lines)]
+    (if row 
+      (* 100 row)
+      (let [column (reflection-row (transpose-lines lines))]
+        (or column 0)))))
 
 (defn day-13a [input]
   (->> input
-       parse-chunks))
+       parse-chunks
+       (map reflection-score)
+       (apply +)))
 
 (defn day-13b [input]
   (->> input))
-
-
-(map str "Hello" "World")
