9d835c0226b739f5408b752c7c8b1e9be04fb591
aatack <alexanderjtatack@gmail.com>
2023-12-15 00:20:48 +0000

    Work out how to tip one rock

--- src/advent_of_code_2023/solutions/day_14.clj
+++ src/advent_of_code_2023/solutions/day_14.clj
@@ -7,14 +7,33 @@
   (let [platform (parse-grid input)]
     {:spaces (->> platform
                   (filter #(#{\O \.} (val %)))
-                  keys)
+                  keys
+                  (into #{}))
      :rocks (->> platform
                  (filter #(#{\O} (val %)))
-                 keys)}))
+                 keys
+                 (into #{}))}))
+
+(defn add [left right]
+  (into [] (map + left right)))
+
+(defn scale [direction magnitude]
+  (into [] (map #(* % magnitude)) direction))
+
+(defn dot [left right]
+  (apply + (map * left right)))
+
+(defn tip-rock [platform direction rock]
+  (let [rocks (disj (:rocks platform) rock)
+        steps (take-while #(let [position (scale direction %)]
+                             (and ((:spaces platform) position) (not (rocks position))))
+                          (range))]
+    (update platform :rocks conj (add rock (scale direction (last steps))))))
 
 (defn day-14a [input]
-  (->> input
-       parse-platform))
+  (-> input
+      parse-platform
+      (tip-rock [1 0] [0 0])))
 
 (defn day-14b [input]
   (->> input))
