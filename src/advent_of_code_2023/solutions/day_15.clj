85e0ac9ddd52a4078660f00ef1e710f3f728869c
aatack <alexanderjtatack@gmail.com>
2023-12-16 12:59:46 +0000

    Implement the hash function

--- src/advent_of_code_2023/solutions/day_15.clj
+++ src/advent_of_code_2023/solutions/day_15.clj
@@ -2,8 +2,15 @@
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
   (:require [clojure.string :refer [split-lines]]))
 
+(defn string-hash [characters]
+  (reduce (fn [value character]
+            (mod (* 17 (+ value (int character))) 256))
+          0
+          characters))
+
 (defn day-15a [input]
-  (->> input))
+  (->> input
+       string-hash))
 
 (defn day-15b [input]
   (->> input))
