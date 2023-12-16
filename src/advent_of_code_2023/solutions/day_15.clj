c89b6be1b05900bbb4ec2dc641069808e0141114
aatack <alexanderjtatack@gmail.com>
2023-12-16 13:18:53 +0000

    Follow the procedure to apply all instructions

--- src/advent_of_code_2023/solutions/day_15.clj
+++ src/advent_of_code_2023/solutions/day_15.clj
@@ -1,7 +1,7 @@
 (ns advent-of-code-2023.solutions.day-15
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
   (:require [advent-of-code-2023.utils :refer [split-string]]
-            [clojure.string :refer [split-lines]]))
+            [clojure.string :refer [includes? split-lines]]))
 
 (defn string-hash [characters]
   (reduce (fn [value character]
@@ -14,6 +14,25 @@
        (split-string ",")
        (map string-hash)
        (apply +)))
+
+(defn apply-instruction [boxes instruction]
+  (if (includes? instruction "-")
+    (let [code (subs instruction 0 (dec (count instruction)))]
+      (update boxes
+              (string-hash code)
+              #(into [] (remove (fn [[key]] (= key code)) (or % [])))))
+    (let [[code value] (split-string "=" instruction)]
+      (update boxes
+              (string-hash code)
+              #(let [modified (into []
+                                    (map (fn [[key focal]]
+                                           (if (= key code)
+                                             [code (read-string value)]
+                                             [key focal]))
+                                         (or % [])))]
+                 (if (= modified (or % []))
+                   (conj modified [code (read-string value)])
+                   modified))))))
 
 (defn day-15b [input]
   (->> input))
