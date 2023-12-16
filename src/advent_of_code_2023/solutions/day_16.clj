628aac29314c096ec2d4154871fd45732626932e
aatack <alexanderjtatack@gmail.com>
2023-12-16 14:47:26 +0000

    Codify the movement of the light

--- src/advent_of_code_2023/solutions/day_16.clj
+++ src/advent_of_code_2023/solutions/day_16.clj
@@ -2,6 +2,34 @@
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
   (:require [clojure.string :refer [split-lines]]))
 
+(defn move [direction [x y]]
+  (case direction
+    :left [(dec x) y]
+    :right [(inc x) y]
+    :up [x (dec y)]
+    :down [x (inc y)]))
+
+(defn propagate [mirror direction]
+  (case mirror
+    \. [direction]
+    \\ (case direction
+         :right [:down]
+         :down [:right]
+         :up [:left]
+         :left [:up])
+    \/ (case direction
+         :right [:up]
+         :down [:left]
+         :up [:right]
+         :left [:down])
+    \| (if (#{:left :right} direction)
+         [:up :down]
+         [direction])
+    \- (if (#{:up :down} direction)
+         [:left :right]
+         [direction])
+    nil []))
+
 (defn day-16a [input]
   (->> input))
 
