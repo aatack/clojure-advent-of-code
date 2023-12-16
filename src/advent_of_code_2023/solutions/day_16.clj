8b90ab208b415f8e3fa4cc45d2bb450ed8c8939d
aatack <alexanderjtatack@gmail.com>
2023-12-16 14:34:37 +0000

    Set up inputs

---
+++ src/advent_of_code_2023/solutions/day_16.clj
@@ -0,0 +1,9 @@
+(ns advent-of-code-2023.solutions.day-16
+  #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
+  (:require [clojure.string :refer [split-lines]]))
+
+(defn day-16a [input]
+  (->> input))
+
+(defn day-16b [input]
+  (->> input))
