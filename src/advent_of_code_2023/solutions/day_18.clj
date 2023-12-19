c5002479215e0afea84bd86277c44d56bc973e5c
aatack <alexanderjtatack@gmail.com>
2023-12-19 22:24:49 +0000

    Parse the input

--- src/advent_of_code_2023/solutions/day_18.clj
+++ src/advent_of_code_2023/solutions/day_18.clj
@@ -1,9 +1,27 @@
 (ns advent-of-code-2023.solutions.day-18
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [clojure.string :refer [split-lines]]))
+  (:require [advent-of-code-2023.utils :refer [split-string]]
+            [clojure.string :refer [split-lines]]))
+
+(defn parse-instructions [input]
+  (->> input
+       split-lines
+       (map #(-> %
+                 (clojure.string/replace #"\(" "")
+                 (clojure.string/replace #"\)" "")
+                 (clojure.string/replace #"#" "")))
+       (map #(split-string " " %))
+       (map (fn [[direction distance colour]]
+              {:direction ({"R" :right
+                            "U" :up
+                            "L" :left
+                            "D" :down} direction)
+               :distance (read-string distance)
+               :colour colour}))))
 
 (defn day-18a [input]
-  (->> input))
+  (->> input
+       parse-instructions))
 
 (defn day-18b [input]
   (->> input))
