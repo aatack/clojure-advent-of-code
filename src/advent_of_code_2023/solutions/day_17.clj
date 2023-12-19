07f915a591e1343e4c61d4fa621ddc884e2c9fe8
aatack <alexanderjtatack@gmail.com>
2023-12-19 00:43:20 +0000

    Track nodes for debugging

--- src/advent_of_code_2023/solutions/day_17.clj
+++ src/advent_of_code_2023/solutions/day_17.clj
@@ -22,7 +22,7 @@
     (fn [node]
       (if (= (:position node) target)
         []
-        (remove nil? [(when (< (:times node) 3)
+        (remove nil? [(when (< (:times node) 2)
                         (let [position (move-direction (:direction node)
                                                        (:position node))
                               loss (blocks position)]
@@ -30,7 +30,8 @@
                             (-> node
                                 (assoc :position position)
                                 (update :times inc)
-                                (update :loss + loss)))))
+                                (update :loss + loss)
+                                (with-meta {:source node})))))
                       (let [direction (turn-direction :clockwise (:direction node))
                             position (move-direction direction (:position node))
                             loss (blocks position)]
@@ -39,7 +40,8 @@
                               (assoc :direction direction)
                               (assoc :position position)
                               (assoc :times 0)
-                              (update :loss + loss))))
+                              (update :loss + loss)
+                              (with-meta {:source node}))))
                       (let [direction (turn-direction :anticlockwise (:direction node))
                             position (move-direction direction (:position node))
                             loss (blocks position)]
@@ -48,14 +50,15 @@
                               (assoc :direction direction)
                               (assoc :position position)
                               (assoc :times 0)
-                              (update :loss + loss))))])))))
+                              (update :loss + loss)
+                              (with-meta {:source node}))))])))))
 
 (defn minimum-heat-loss [blocks explore]
   (loop [queue #{initial-node (assoc initial-node :direction :down)}
          index {}]
     (if (or (empty? queue) #_(> (count index) 3000))
-      (reverse (sort-by #(vector (apply + (-> % :position))
-                                 (* -1 (:loss %))) (mapcat second index)))
+      (first (reverse (sort-by #(vector (apply + (-> % :position))
+                                        (* -1 (:loss %))) (mapcat second index))))
       (let [node (first queue)]
         (recur (apply conj (disj queue node)
                       (filter (fn [candidate]
@@ -74,11 +77,16 @@
                         (abs (- (second position) row)))]
         (+ (* -1 distance) (* -0.1 loss))))))
 
+(defn track [node]
+  (if node
+    (conj (track (-> node meta :source)) node)
+    []))
+
 (defn day-17a [input]
   (let [blocks (parse-blocks input)
         explore (explore-node blocks)
         evaluate (evaluate-node blocks)]
-    (minimum-heat-loss blocks explore)))
+    (track (minimum-heat-loss blocks explore))))
 
 (defn day-17b [input]
   (->> input))
