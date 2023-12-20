73cdd58f7ada4bfbdf5ca7bd62e0ccc14ad71ce3
aatack <alexanderjtatack@gmail.com>
2023-12-20 00:18:09 +0000

    Solve the first part

--- src/advent_of_code_2023/solutions/day_18.clj
+++ src/advent_of_code_2023/solutions/day_18.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2023.solutions.day-18
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [advent-of-code-2023.utils :refer [move-direction split-string]]
+  (:require [advent-of-code-2023.solutions.day-16 :refer [explore]]
+            [advent-of-code-2023.utils :refer [move-direction split-string]]
             [clojure.string :refer [split-lines]]))
 
 (defn parse-instructions [input]
@@ -29,30 +30,17 @@
            (mapcat #(repeat (:distance %) %) instructions))
    (into #{})))
 
-(defn count-segment-interior
-  "For a given slice of a trench, determine the number of interior segments.
-   
-   This includes the trench itself.  The trench positions should be given as a sequence
-   of integers, representing the y-coordinates of each section of trench on a given
-   x-coordinate."
-  [trench-positions]
-  (->> (partition 3 1 [nil] (cons nil trench-positions))
-       (remove (fn [[a b c]] (= (inc (or a b)) b (dec (or c b)))))))
-
-(count-segment-interior '(0 1 2 5 6 7))
-
 (defn determine-interior [trench]
-  (->> trench
-       (group-by first)
-       vals
-       (map #(sort (map second %)))
-       (map count-segment-interior)))
+  (explore (fn [node]
+             (remove trench [(move-direction :up node)
+                             (move-direction :down node)
+                             (move-direction :left node)
+                             (move-direction :right node)]))
+           [1 1]))
 
 (defn day-18a [input]
-  (->> input
-       parse-instructions
-       build-trench
-       determine-interior))
+  (let [trench (->> input parse-instructions build-trench)]
+    (+ (count trench) (count (determine-interior trench)))))
 
 (defn day-18b [input]
   (->> input))
