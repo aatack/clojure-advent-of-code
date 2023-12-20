8a1d2ae71677edcba9eaf9ed3b9b11a35e075026
aatack <alexanderjtatack@gmail.com>
2023-12-20 00:04:27 +0000

    Split the interior up into segments

--- src/advent_of_code_2023/solutions/day_18.clj
+++ src/advent_of_code_2023/solutions/day_18.clj
@@ -22,17 +22,37 @@
 (defn build-trench [instructions]
   (->>
    (reduce (fn [trench instruction]
-            (cons (with-meta (move-direction (:direction instruction) (first trench))
-                    {:instruction instruction})
-                  trench))
-          '([0 0])
-          (mapcat #(repeat (:distance %) %) instructions))
+             (cons (with-meta (move-direction (:direction instruction) (first trench))
+                     {:instruction instruction})
+                   trench))
+           '([0 0])
+           (mapcat #(repeat (:distance %) %) instructions))
    (into #{})))
+
+(defn count-segment-interior
+  "For a given slice of a trench, determine the number of interior segments.
+   
+   This includes the trench itself.  The trench positions should be given as a sequence
+   of integers, representing the y-coordinates of each section of trench on a given
+   x-coordinate."
+  [trench-positions]
+  (->> (partition 3 1 [nil] (cons nil trench-positions))
+       (remove (fn [[a b c]] (= (inc (or a b)) b (dec (or c b)))))))
+
+(count-segment-interior '(0 1 2 5 6 7))
+
+(defn determine-interior [trench]
+  (->> trench
+       (group-by first)
+       vals
+       (map #(sort (map second %)))
+       (map count-segment-interior)))
 
 (defn day-18a [input]
   (->> input
        parse-instructions
-       build-trench))
+       build-trench
+       determine-interior))
 
 (defn day-18b [input]
   (->> input))
