0c8dc4685efae95aa6be78d612ed7d7df9cf8959
aatack <alexanderjtatack@gmail.com>
2023-12-22 15:09:01 +0000

    Compute the answer analytically instead

--- src/advent_of_code_2023/solutions/day_18.clj
+++ src/advent_of_code_2023/solutions/day_18.clj
@@ -1,6 +1,7 @@
 (ns advent-of-code-2023.solutions.day-18
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [advent-of-code-2023.solutions.day-16 :refer [explore]]
+  (:require [advent-of-code-2023.solutions.day-14 :refer [add scale]]
+            [advent-of-code-2023.solutions.day-16 :refer [explore]]
             [advent-of-code-2023.utils :refer [move-direction split-string]]
             [clojure.string :refer [split-lines]]))
 
@@ -20,27 +21,28 @@
                :distance (read-string distance)
                :colour colour}))))
 
-(defn build-trench [instructions]
-  (->>
-   (reduce (fn [trench instruction]
-             (cons (with-meta (move-direction (:direction instruction) (first trench))
-                     {:instruction instruction})
-                   trench))
-           '([0 0])
-           (mapcat #(repeat (:distance %) %) instructions))
-   (into #{})))
+(defn build-vertices [instructions]
+  (reductions (fn [vertex {:keys [direction distance]}]
+                (add vertex (scale (move-direction direction [0 0]) distance)))
+              [0 0]
+              instructions))
 
-(defn determine-interior [trench]
-  (explore (fn [node]
-             (remove trench [(move-direction :up node)
-                             (move-direction :down node)
-                             (move-direction :left node)
-                             (move-direction :right node)]))
-           [1 1]))
+(defn polygon-area [vertices]
+  (->> vertices
+       (partition 2 1)
+       (map (fn [[[left-x left-y] [right-x right-y]]]
+              (let [dx (- right-x left-x)
+                    dy (- right-y left-y)]
+                (* (+ (* (abs dx) (min left-y right-y))
+                      (/ (* (abs dx) (abs dy)) 2))
+                   (if (< dx 0) 1 -1)))))
+       (apply +)
+       abs))
 
 (defn day-18a [input]
-  (let [trench (->> input parse-instructions build-trench)]
-    (+ (count trench) (count (determine-interior trench)))))
+  (let [instructions (->> input parse-instructions)]
+    (+ (->> instructions build-vertices polygon-area)
+       (->> instructions (map :distance) (apply +) (* 0.5) int inc))))
 
 (defn day-18b [input]
   (->> input))
