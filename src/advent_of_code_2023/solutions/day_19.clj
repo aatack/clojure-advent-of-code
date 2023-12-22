24710ae65e532d1c048394ec1b28b46bfce353a6
aatack <alexanderjtatack@gmail.com>
2023-12-22 15:50:27 +0000

    Parse all the relevant bits

--- src/advent_of_code_2023/solutions/day_19.clj
+++ src/advent_of_code_2023/solutions/day_19.clj
@@ -1,19 +1,47 @@
 (ns advent-of-code-2023.solutions.day-19
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
   (:require [advent-of-code-2023.parsing :refer [parse-chunks]]
-            [clojure.string :refer [split-lines]]))
+            [advent-of-code-2023.utils :refer [split-string]]
+            [clojure.string :refer [includes? split-lines]]))
+
+(defn parse-instruction [instruction]
+  (let [guarded-instruction (if (includes? instruction ":")
+                              instruction
+                              (str "x>0:" instruction))
+        [condition result] (split-string ":" guarded-instruction)]
+    {:greater (= (second condition) \>)
+     :variable (-> condition first str read-string)
+     :value (-> condition (subs 2) read-string)
+     :result result}))
 
 (defn parse-workflow [workflow]
-  workflow)
+  (let [[name instructions] (split-string "\\{" (subs workflow 0 (dec (count workflow))))]
+    [name (map parse-instruction (split-string "," instructions))]))
 
 (defn parse-part [part]
   (read-string (clojure.string/replace part #"=" " ")))
 
+(defn accepted? [workflows part]
+  (loop [steps (workflows "in")]
+    (let [{:keys [greater variable value result]} (first steps)
+          workflow (when ((if greater > <) (part variable) value) result)]
+      (case workflow
+        nil (recur (rest steps))
+        "A" true
+        "R" false
+        (recur (workflows workflow))))))
+
+(defn score [part]
+  (->> part vals (apply +)))
+
 (defn day-19a [input]
   (let [[workflows-input parts-input] (parse-chunks input)
         parts (map parse-part parts-input)
-        workflows (map parse-workflow workflows-input)]
-    parts))
+        workflows (into {} (map parse-workflow workflows-input))]
+    (->> parts
+         (filter #(accepted? workflows %))
+         (map score)
+         (apply +))))
 
 (defn day-19b [input]
   (->> input))
