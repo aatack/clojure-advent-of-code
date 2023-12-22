688dd2550cbdacf7af4614565862dfa106fd0416
aatack <alexanderjtatack@gmail.com>
2023-12-22 18:09:54 +0000

    Define the basic propagation behaviour

--- src/advent_of_code_2023/solutions/day_20.clj
+++ src/advent_of_code_2023/solutions/day_20.clj
@@ -21,6 +21,29 @@
          "button"
          {:state nil :outputs '("broadcaster")}))
 
+(defn fire [module strength]
+  [(:state module) strength])
+
+(defn send-signals [initial-modules initial-sources]
+  (loop [signals []
+         sources initial-sources
+         modules initial-modules
+         pulses {:low 0 :high 0}]
+    (if (empty? signals)
+      (if (empty? sources)
+        pulses
+        (recur [(first sources)]
+               (rest sources)
+               modules
+               pulses))
+      (let [[source input-strength] (first signals)
+            [state output-strength] (fire (modules source) input-strength)]
+        (recur (apply conj (rest signals) (map (fn [child] [child output-strength])
+                                               (get-in modules [source :children])))
+               sources
+               (assoc-in modules [source :state] state)
+               (update pulses input-strength inc))))))
+
 (defn day-20a [input]
   (->> input
        parse-modules))
