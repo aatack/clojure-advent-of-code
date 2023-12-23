b07f75c2ba8eb85e9c41e383cd9c045cba42651f
aatack <alexanderjtatack@gmail.com>
2023-12-23 22:41:47 +0000

    Provisionally watch various inputs

--- src/advent_of_code_2023/solutions/day_20.clj
+++ src/advent_of_code_2023/solutions/day_20.clj
@@ -75,44 +75,57 @@
        vals
        (apply *)))
 
-(defn press-button [initial-modules]
-  (loop [signals [["button" :low "broadcaster"]]
-         modules initial-modules
-         presses 0
-         receivers #{}]
-    (if (empty? signals)
-      (with-meta modules {:receivers receivers})
-      (let [[source input-strength destination] (first signals)
-            [state output-strength] (fire source (modules destination) input-strength)]
-        (recur (apply conj (into [] (rest signals))
-                      (if (nil? output-strength)
-                        []
-                        (map (fn [child] [destination output-strength child])
-                             (get-in modules [destination :outputs]))))
-               (assoc-in modules [destination :state] state)
-               presses
-               (if (= input-strength :low) (conj receivers destination) receivers))))))
+(defn press-button
+  ([initial-modules] (press-button initial-modules #{}))
+  ([initial-modules watch]
+   (loop [signals [["button" :low "broadcaster"]]
+          modules initial-modules
+          step 0
+          results {}]
+     (if (empty? signals)
+       (with-meta modules {:results results})
+       (let [[source input-strength destination] (first signals)
+             [state output-strength] (fire source (modules destination) input-strength)]
+         (recur (apply conj (into [] (rest signals))
+                       (if (nil? output-strength)
+                         []
+                         (map (fn [child] [destination output-strength child])
+                              (get-in modules [destination :outputs]))))
+                (assoc-in modules [destination :state] state)
+                (inc step)
+                (if (watch source)
+                  (update results source #(conj (or % []) [step input-strength]))
+                  results)))))))
 
-(defn invert-modules [modules]
-  (->> modules
-       (map-vals :outputs)
-       (mapcat (fn [[key outputs]] (map #(vector % key) outputs)))
-       (group-by first)
-       (map-vals #(map second %))))
-
-(defn day-20b [input]
+(defn day-20b
+  "Determine the solution to the second part.
+   
+   This works by identifying subgraphs with no circular dependencies.  If a subgraph has
+   no circular dependencies, it follows that - given an unchanging source of inputs from
+   the button - the behaviour of that subgraph must be cyclical (from the pidgeonhole
+   principle: there are only a certain number of states that the subgraph can be in, so
+   they must eventually repeat).
+   
+   First we identify all subgraphs with no circular dependencies, then we determine the
+   cycle duration for each one.  **We then make the assumption that each of these nodes
+   feeds into a single conjunction node, which then feeds out into the `rx` node.**
+   Therefore we can treat each node's last input as its current input, and need only
+   find the period of time for which each subgraph node is outputting a high signal
+   strength.  Then we find a time where each of them are outputting a high signal
+   strength simultaneously."
+  [input]
   (let [graph (parse-modules input)
-        subgraph-nodes (->> graph
-                            (map #(vector (first %) (node-inputs graph (first %))))
-                            (filter second)
-                            (map first))
+        subgraph-nodes (disj (->> graph
+                                  (map #(vector (first %) (node-inputs graph (first %))))
+                                  (filter second)
+                                  (map first)
+                                  set)
+                             "button" "broadcaster")
         history (doall (->> graph (iterate press-button) (take 10000)))]
     (->> (for [node subgraph-nodes]
            [node (->> history
                       (map #(node-subgraph % node))
                       find-cycle
                       (#(select-keys % [:first :second])))])
-         (filter #(and (not-empty (second %))
-                       (not= (first %) "broadcaster")
-                       (not= (first %) "button")))
+         (filter #(not-empty (second %)))
          (into {}))))
