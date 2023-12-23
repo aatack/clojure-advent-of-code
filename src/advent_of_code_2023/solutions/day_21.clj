a27285b5c98953955e4f360e72cf29d1bdb9dcbc
aatack <alexanderjtatack@gmail.com>
2023-12-23 23:26:04 +0000

    Set up a step function

---
+++ src/advent_of_code_2023/solutions/day_21.clj
@@ -0,0 +1,39 @@
+(ns advent-of-code-2023.solutions.day-21
+  #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
+  (:require [advent-of-code-2023.parsing :refer [parse-grid]]
+            [advent-of-code-2023.utils :refer [move-direction]]
+            [clojure.string :refer [split-lines]]))
+
+(defn parse-plot [input]
+  (let [plot (parse-grid input)]
+    {:unreachable (->> plot
+                       (filter #(= (second %) \.))
+                       keys
+                       (into #{}))
+     :reachable (->> plot
+                     (filter #(= (second %) \S))
+                     keys
+                     (into #{}))}))
+
+(defn north-east-south-west [position]
+  (map #(move-direction % position) [:up :right :down :left]))
+
+(defn step [plot]
+  (assoc plot
+         :reachable
+         (->> plot
+              :reachable
+              (mapcat north-east-south-west)
+              (filter (:unreachable plot)))))
+
+(defn day-21a [input]
+  (->> input
+       parse-plot
+       (iterate step)
+       (drop 2)
+       first
+       :reachable
+       count))
+
+(defn day-21b [input]
+  (->> input))
