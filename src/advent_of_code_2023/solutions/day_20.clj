f5d7cb6a2ce623f1821be76a0dcfaa31cef72b88
aatack <alexanderjtatack@gmail.com>
2023-12-23 23:05:48 +0000

    Find and document the solution

--- src/advent_of_code_2023/solutions/day_20.clj
+++ src/advent_of_code_2023/solutions/day_20.clj
@@ -1,7 +1,9 @@
 (ns advent-of-code-2023.solutions.day-20
   #_{:clj-kondo/ignore [:unused-referred-var :unused-namespace]}
-  (:require [advent-of-code-2023.graphs :refer [build-graph node-inputs
+  (:require [advent-of-code-2022.utils :refer [enumerate]]
+            [advent-of-code-2023.graphs :refer [build-graph node-inputs
                                                 node-subgraph]]
+            [advent-of-code-2023.solutions.day-08 :refer [lowest-common-multiple]]
             [advent-of-code-2023.utils :refer [find-cycle map-vals
                                                split-string]]
             [clojure.string :refer [split-lines]]))
@@ -112,7 +114,26 @@
    Therefore we can treat each node's last input as its current input, and need only
    find the period of time for which each subgraph node is outputting a high signal
    strength.  Then we find a time where each of them are outputting a high signal
-   strength simultaneously."
+   strength simultaneously.
+   
+   As it transpires, all nodes emit a high signal strength for an overlapping period
+   within exactly one step in the cycle.  The full information from this can be seen in
+   the `:activation` key of the output map.  Because the periods for which the nodes
+   are emitting a high signal strength overlap within the button press, we need only
+   determine when the button presses (ie. outer loops instead of inner loops) will
+   overlap.
+   
+   It is assumed that this overlap period holds true for all puzzle inputs.  It is also
+   assumed that, for all puzzle inputs, the button press within which the high-strength
+   pulse occurs is in the `n - 1`th button press of every cycle, where `n` is the number
+   of states in the cycle.
+   
+   Hence, also assuming that each cycle starts on the second button press, we need only
+   find the lowest common multiple of the cycle lengths of the subgraph nodes.  (The
+   cycles will all end at the same time on the button press whose number is the lowest
+   common multiple plus one - because the cycles don't start until the second button
+   press.  Then the high output occurs one press *before* the end of each cycle; the two
+   cancel out.)"
   [input]
   (let [graph (parse-modules input)
         subgraph-nodes (disj (->> graph
@@ -129,8 +150,20 @@
                             (iterate #(press-button % subgraph-nodes))
                             (take 5000)))]
     (->> (for [node subgraph-nodes]
-           [node (->> history
-                      (map #(node-subgraph % node))
-                      find-cycle
-                      (#(select-keys % [:first :second])))])
-         (into {}))))
+           [node {:cycle (->> history
+                              (map #(node-subgraph % node))
+                              find-cycle
+                              (#(select-keys % [:first :second])))
+                  :activation (->> (for [[outer-index results]
+                                         (->> history
+                                              rest
+                                              (map (comp #(get % node) :results meta))
+                                              enumerate)
+                                         [inner-index state] results]
+                                     [[outer-index inner-index] state])
+                                   (partition-by second)
+                                   (map first))}])
+         (into {})
+         (map #(get-in % [1 :cycle]))
+         (map (fn [{:keys [first second]}] (- second first)))
+         (apply lowest-common-multiple))))
